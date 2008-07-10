d807e46e5971b4d12ed52328f3cf15bf2edd39fc
Chen Jin <cgj>
2008-07-10 03:28:10 +0000

    taking care c-language bugs

--- utils/gwrite/xmlparser.py
+++ utils/gwrite/xmlparser.py
@@ -6,18 +6,22 @@ from time import sleep
 
 items=""
 nindent=0
-def processvar(node,language):
+language_flag=0
+def processvar(node,language_sw,coord_comm,coord_var):
+    line="" 
     varname=str(node.getAttribute("name"))
-    if (language=="Fortran"):
+    if (language_sw==1):
         if(node.getAttribute("gname")!=""):
            varname_dim=node.getAttribute("gname")
         else:
            varname_dim=varname
         if(node.getAttribute("goffset")!=""):
            varname_dim=varname_dim+"("+str(node.getAttribute("goffset")) +")"
-        #else:
-  	#   varname_dim=varname
-    elif(language=="C" or language=="c" or language=="cpp" or language=="CPP"):
+        if(coord_comm==varname or coord_var==varname):
+           line="call adios_write(aaaabbbb,"+"\""+varname+"\"//char(0),"+varname_dim+")"
+        else:
+           line="call adios_op(aaaabbbb,"+"\""+varname+"\"//char(0),"+varname_dim+")"
+    elif(language_sw==2):
         if(node.getAttribute("gname")!=""):
            varname_dim=node.getAttribute("gname")
         else:
@@ -28,68 +32,47 @@ def processvar(node,language):
            varname_dim=varname_dim
         else:
            varname_dim="&"+varname_dim
-    line="call adios_write(gid,"+"\""+varname+"\"//char(0),"+varname_dim+")"
+        if(coord_comm==varname or coord_var==varname):
+           line="adios_write(aaaabbbb,"+"\""+varname+"\","+varname_dim+");"
+        else:
+           line="adios_op(aaaabbbb,"+"\""+varname+"\","+varname_dim+");"
     return line
 
-
-def processnode(nodelist,language):
+def processnode(nodelist,language_sw,coord_comm,coord_var):
    global items
-#   global nindent
    for node in nodelist:
        if (node.nodeType==node.ELEMENT_NODE and node.nodeName=="var"):
-#           indent=""
-#           for i in range(nindent): 
-#               indent=indent + "\t"
-#           items=items+"\n"+indent+processvar(node,language)
-           items=items+processvar(node,language)+"\n"
-       elif (node.nodeType==node.ELEMENT_NODE and node.nodeName=="condition"):
-           varname=str(node.getAttribute("expression"))
-#           indent=""
-#           varname=str(node.getAttribute("begin_expression"))
-#           if (varname!=""):
-#                for i in range(nindent):
-#                    indent=indent+"\t"
-#                nindent=nindent+1
-#           else:
-#                varname=str(node.getAttribute("end_expression"))
-#                if (varname!=""):
-#                    nindent=nindent-1
-#                    for i in range(nindent):
-#                        indent=indent+"\t"
-#           items=items+"\n"+indent+varname
+           items=items+processvar(node,language_sw,coord_comm,coord_var)+"\n"
+       elif (node.nodeType==node.ELEMENT_NODE and node.nodeName=="gwrite"):
+           varname=str(node.getAttribute("src"))
            items=items+varname+"\n"
        subnodelist=node.childNodes
-       processnode(subnodelist,language)
+       processnode(subnodelist,language_sw,coord_comm,coord_var)
  
 def getVarlistFromXML(xmlFile):
-#    print xmlFile
     global items 
+    global language_flag 
 #    if ( os.path.isfile (xmlFile)):
     variables={}
-    #items={}
     doc=xml.dom.minidom.parse(xmlFile)# parse an XML file by name
     idx=0
     group=doc.getElementsByTagName("adios-config")
     language=(group[0].getAttribute("host-language"))
+    
+    if (language=="Fortran"):
+	 language_flag=1
+    elif(language=="C" or language=="c" or language=="cpp" or language=="CPP"):
+         language_flag=2
     for group in doc.getElementsByTagName("adios-group"):
          items=""
          indent=""
          gname=group.getAttribute("name")
+         coord_comm=group.getAttribute("coordination-communicator")
+         coord_var=group.getAttribute("coordination-var")
          nodelist=group.childNodes
-         #for node in nodelist:
-         processnode(nodelist,language)
+         processnode(nodelist,language_flag,coord_comm,coord_var)
          variables[str(gname)]=items
-         #print items
     return variables
-#	     if (node.nodeType==node.ELEMENT_NODE and node.nodeName=="var"):
-#		 items=items+"\n"+processvar(node,language)
-#	     elif (node.nodeType==node.ELEMENT_NODE and node.nodeName=="condition"):
-#                varname=str(node.getAttribute("expression"))
-#                items=items+"\n"+varname
-#                subnodelist=node.childNodes
-#                for subnode in subnodelist:
-#         	    if (subnode.nodeType==subnode.ELEMENT_NODE and subnode.nodeName=="var"):
-#		        items=items+"\n\t"+processvar(node,language)
 
 def main(argv=None):
     if argv is None:
