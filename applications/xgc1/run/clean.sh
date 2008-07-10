2dda79bf495498e2dd8b8e9184bee576fa568a36
Chen Jin <cgj>
2008-07-10 20:41:19 +0000

    cgj

---
+++ applications/xgc1/run/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FILES="staging_nodes.* fort.used.eq fort.input.used xgc.flowdiag.cdf fort.bp fort.103 fort.pot_zero fort.50 fort.30 fort.700 results.dat xgc.fluxdiag.cdf netcdfflow.bp conf *.bp stderr p2t_*.log"
+
+rm -f ${FILES}
