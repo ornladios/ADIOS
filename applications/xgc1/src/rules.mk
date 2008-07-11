75cbfdd50e214da8a70e626b7686581a7bce2394
Chen Jin <cgj>
2008-07-11 02:37:17 +0000

    cgj

--- applications/xgc1/src/rules.mk
+++ applications/xgc1/src/rules.mk
@@ -1,10 +1,10 @@
 $(CMD): $(OBJ) $(BINPACK_LIB)
-	$(CMP) -o $(CMD) $(OPT) $(OBJ) $(LIB) -L${PETSC_DIR}/lib/${PETSC_ARCH} -lpetsc ${PETSC_FORTRAN_LIB} ${PETSC_KSP_LIB}
+	$(CMP) -o $(CMD) $(OPT) $(OBJ) $(LIB) -L${PETSC_DIR}/lib/${PETSC_ARCH} -lcraypetsc ${PETSC_FORTRAN_LIB} ${PETSC_KSP_LIB}
 
 $(OBJ): module.F90
 
 .F90.o : config.xml
-	python gwrite.py temp
+	python gwrite.py config.xml temp
 	$(CMP) $(OPT) -I${PETSC_DIR} -I${PETSC_DIR}/bmake/${PETSC_ARCH} -I${PETSC_DIR}/include -I${PETSC_DIR}/include/finclude ${MPI_INCLUDE} -c temp/$<
 
 clean:
