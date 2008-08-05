2661dc595f9231633e244e589e557922fd14f142
Chen Jin <cgj>
2008-08-05 14:19:59 +0000

    cgj: add the adios_group_size and can be preprocessed without parsing the source code.

--- utils/gwrite/xmlparser.py
+++ utils/gwrite/xmlparser.py
@@ -9,11 +9,14 @@ items=""
 sizeformular=[]
 sizestr={}
 getsize=gwrite_types.adios_size
-language_flag=0
-groupdict={}
+var_gname_dict={}
+language_group_dict={}
+var_size_dict={}
+
 def processvar(node,language_sw,coord_comm,coord_var):
     global sizeformular
-    global groupdict 
+    global var_gname_dict 
+    global var_size_dict 
     line=""
 ########################################################          
 # Modified Code: Case Insensitive
@@ -24,6 +27,7 @@ def processvar(node,language_sw,coord_comm,coord_var):
     pathname="" 
     dimsname=""
     varname_g=""
+    copyflag=""
     for akey in attkeys:
         akeystr=str(akey).lower()
         if(akeystr=="dimensions"):
@@ -36,64 +40,105 @@ def processvar(node,language_sw,coord_comm,coord_var):
            varname=node.attributes[akey].value
         elif(akeystr=="gname"):
            varname_g=node.attributes[akey].value
+        elif(akeystr=="copy-on-write"):
+           copyflag=node.attributes[akey].value
     if(varname=="" or typename==""):
        print "Warning: empty varname or type for adiosgroup: "+node.nodeName
-    if (dimsname!=""):
-        return line
-    #print varname,dimsname,typename,varname_g 
-    strsize=str(getsize[typename])
-    sizeformular.append(strsize)
     if(varname_g==""):
        varname_g=varname
-    if(groupdict.has_key(varname)):
-       if(pathname[len(pathname)-1]!='/'):
-          varname=pathname+'/'+varname 
-       groupdict[varname]=varname_g
+    if(var_gname_dict.has_key(varname)):
+       if(pathname!="" and pathname[len(pathname)-1]!='/'):
+          varname=pathname+'/'+varname
+       var_gname_dict[varname]=varname_g
     else:
-       groupdict[varname]=varname_g
+       var_gname_dict[varname]=varname_g
 
-########################################################          
-# Original Code: Case Sensitive
-########################################################          
-    #dimsname=node.getAttribute("dimensions")
-    #typename=node.getAttribute("type")
-    #varname=str(node.getAttribute("name"))
-    #if(node.getAttribute("gname")!=""):
-    #   varname_g=node.getAttribute("gname")
-    #else:
-    #   varname_g=varname
-########################################################          
-           
+# Add var-size-mapping   
+    line="" 
+    if(dimsname==""):
+       line=str(getsize[typename])
+    else:
+       str_varsize=str(getsize[typename])
+       dimsarr=dimsname.split(',');
+       for dimsele in dimsarr:
+         if(dimsele==dimsarr[0]):
+            if(dimsele.isdigit()):
+               line=str_varsize+'*'+'('+dimsele+')'
+            else:
+               line=str_varsize+'*'+'('+var_gname_dict[dimsele]+')'
+         else:
+            if(dimsele.isdigit()):
+               line=line+'*'+'('+dimsele+')'
+            else:
+               line=line+'*'+'('+var_gname_dict[dimsele]+')'
+       if(str(node.getAttribute("copy-on-write")).lower()=="yes"):
+          line ='2*'+'('+line+')'
+    sizeformular.append(line)    
+    var_size_dict[varname]=line
+    line=""
     if (language_sw==1):
         if(coord_comm==varname or coord_var==varname): 
-           line="call adios_write(aaaabbbb,"+"\""+varname+"\"//char(0),"+varname_g+")"
+           line="call adios_write(_handle,"+"\""+varname+"\"//char(0),"+varname_g+")"
         else:
-           line="call adios_op(aaaabbbb,"+"\""+varname+"\"//char(0),"+varname_g+")"
+           line="call adios_op(_handle,"+"\""+varname+"\"//char(0),"+varname_g+")"
     elif(language_sw==2):
         if(coord_comm==varname or coord_var==varname):
-           line="adios_write(aaaabbbb,"+"\""+varname+"\","+"&"+varname_g+");"
+           line="adios_write(_handle,"+"\""+varname+"\","+"&"+varname_g+");"
         else:
            for c in varname_g:
                if(c=='+' or c=='-' or c=='*' or c=='/' or c=='^' or c=='%'):
                   print "Fatal: var --"+varname_g+"-- cannot be written"
                   raise SystemExit
-           line="adios_op(aaaabbbb,"+"\""+varname+"\","+"&"+varname_g+");"
+           line="adios_op(_handle,"+"\""+varname+"\","+"&"+varname_g+");"
     return line+'\n'
 
 def processattr(node,language_sw):
-    line=""
-    return line
+    global sizeformular
+    global var_gname_dict
+    global var_size_dict
+    attkeys=node.attributes.keys()
+    for akey in attkeys:
+        akeystr=str(akey).lower()
+        if(akeystr=="value"):
+           return 
+        elif(akeystr=="var"):
+           varname=node.attributes[akey].value
+           if(var_size_dict.has_key(varname)):
+              sizeformular.append(var_size_dict[varname]) 
+           else:
+              print "\""+varname+"\" is not declared before the attribute \""+node.attributes["name"].value+"\""
+
 
 def processdset(node,language_sw):
     global sizeformular
-    global groupdict
+    global var_gname_dict
+    global var_size_dict
     line=""
-
+    attkeys=node.attributes.keys()
+    varname=""
+    typename="" 
+    dimsname=""
+    varname_g=""
+    copyflag=""
+    for akey in attkeys:
+        akeystr=str(akey).lower()
+        if(akeystr=="dimensions"):
+           dimsname=node.attributes[akey].value
+        elif(akeystr=="type"):
+           typename=node.attributes[akey].value
+        elif(akeystr=="name"):
+           varname=node.attributes[akey].value
+        elif(akeystr=="gname"):
+           varname_g=node.attributes[akey].value
+        elif(akeystr=="copy-on-write"):
+           copyflag=node.attributes[akey].value
+   
 ########################################################          
 # Modified Code: Case Insensitive
 ########################################################          
     attkeys=node.attributes.keys()
     varname=""
+    pathname=""
     typename="" 
     dimsname=""
     varname_g=""
@@ -114,36 +159,45 @@ def processdset(node,language_sw):
         return line 
     if(varname_g==""):
        varname_g=varname
+    if(var_gname_dict.has_key(varname)):
+       if(pathname!="" and pathname[len(pathname)-1]!='/'):
+          varname=pathname+'/'+varname 
+       var_gname_dict[varname]=varname_g
+    else:
+       var_gname_dict[varname]=varname_g
+    
 ########################################################          
 # Original Code: Case Sensitive
 ########################################################          
 #    dimsname=node.getAttribute("dimensions")
 #    typename=node.getAttribute("type")
 ########################################################          
-    strsize=str(getsize[typename])
+    str_varsize=str(getsize[typename])
     dimsarr=dimsname.split(',');
     for dimsele in dimsarr:
            
         if(dimsele==dimsarr[0]):
            if(dimsele.isdigit()):
-              line=strsize+'*'+'('+dimsele+')'
+              line=str_varsize+'*'+'('+dimsele+')'
            else:
-              line=strsize+'*'+'('+groupdict[dimsele]+')'
+              line=str_varsize+'*'+'('+var_gname_dict[dimsele]+')'
         else:
            if(dimsele.isdigit()):
               line=line+'*'+'('+dimsele+')'
            else:
-              line=line+'*'+'('+groupdict[dimsele]+')'
+              line=line+'*'+'('+var_gname_dict[dimsele]+')'
    
     if(str(node.getAttribute("copy-on-write")).lower()=="yes"):
            line ='2*'+'('+line+')'
     
     sizeformular.append(line)
+    var_size_dict[varname]=line
+
     line=""
     if (language_sw==1):
-        line="call adios_op(aaaabbbb,"+"\""+varname+"\"//char(0),"+varname_g+")"
+        line="call adios_op(_handle,"+"\""+varname+"\"//char(0),"+varname_g+")"
     elif(language_sw==2):
-        line="adios_op(aaaabbbb,"+"\""+varname+"\","+varname_g+");"
+        line="adios_op(_handle,"+"\""+varname+"\","+varname_g+");"
     return line+'\n'
 
 def processnode(nodelist,language_sw,coord_comm,coord_var):
@@ -152,22 +206,25 @@ def processnode(nodelist,language_sw,coord_comm,coord_var):
    for node in nodelist:
        if (node.nodeType==node.ELEMENT_NODE and node.nodeName.lower()=="var"):
            items=items+processvar(node,language_sw,coord_comm,coord_var)
-       subnodelist=node.childNodes
-       processnode(subnodelist,language_sw,coord_comm,coord_var)
-# process dataset-type var  
-   for node in nodelist:
-       if (node.nodeType==node.ELEMENT_NODE and node.nodeName.lower()=="var"):
-           items=items+processdset(node,language_sw)
-       elif (node.nodeType==node.ELEMENT_NODE and node.nodeName.lower()=="attribute"):
-           items=items+processattr(node,language_sw)
        elif (node.nodeType==node.ELEMENT_NODE and node.nodeName.lower()=="gwrite"):
            varname=str(node.getAttribute("src"))
            items=items+varname+"\n"
        subnodelist=node.childNodes
        processnode(subnodelist,language_sw,coord_comm,coord_var)
+# process dataset-type var  
+#   for node in nodelist:
+#       if (node.nodeType==node.ELEMENT_NODE and node.nodeName.lower()=="var"):
+#           items=items+processdset(node,language_sw)
+#       subnodelist=node.childNodes
+#       processnode(subnodelist,language_sw,coord_comm,coord_var)
+   for node in nodelist:
+       if (node.nodeType==node.ELEMENT_NODE and node.nodeName.lower()=="attribute"):
+           processattr(node,language_sw)
+       subnodelist=node.childNodes
+       #processnode(subnodelist,language_sw,coord_comm,coord_var)
 
 def getVarlistFromXML(xmlFile):
-    global items,sizeformular, language_flag 
+    global items,sizeformular, language_group_dict 
 #    if ( os.path.isfile (xmlFile)):
     variables={}
     doc=xml.dom.minidom.parse(xmlFile)# parse an XML file by name
@@ -205,6 +262,7 @@ def getVarlistFromXML(xmlFile):
             items=""
             indent=""
             attkeys=group.attributes.keys()
+            glanguage=language_flag
             for akey in attkeys:
                 akeystr=str(akey).lower()
                 if (akeystr=="name"):
@@ -213,32 +271,30 @@ def getVarlistFromXML(xmlFile):
                     coord_comm=group.attributes[akey].value
                 elif (akeystr=="coordination-var"):
                     coord_var=group.attributes[akey].value
+                elif (akeystr=="host-language"):
+                    language=group.attributes[akey].value
+                    if (language.lower()=="fortran"):
+               	        glanguage=1
+                    elif(language.lower()=="c" or language.lower()=="cpp"):
+                        glanguage=2
+                    else:
+                        glanguage==-1
+                        print "Fatal: Unknown language supported!"
+                        raise SystemExit
                 else:
                     print "Warning: Unknown attribute --"+str(akey)+"-- for adios-group: "+gname
             if (gname ==""):
                 print "Fatal: name for adios-group is required!"
                 raise SystemExit
             nodelist=group.childNodes
-            processnode(nodelist,language_flag,coord_comm,coord_var)
-            groupdict={}
+            processnode(nodelist,glanguage,coord_comm,coord_var)
+            var_gname_dict={}
+            language_group_dict[gname]=glanguage
             variables[str(gname)]=items
-            sizestr[str(gname)] = sizeformular    
+            sizestr[str(gname)] = sizeformular   
             sizeformular=[]
     return variables
 
-def processgroup(group,gname,coord_comm,coord_var):
-    attkeys=group.attributes.keys()
-    for akey in attkeys:
-        akeystr=str(akey).lower()
-        if (akeystr=="name"):
-            gname=group.attributes[akey].value
-        elif (akeystr=="coordination-communicator"):
-            coord_comm=group.attributes[akey].value
-        elif (akeystr=="coordination-var"):
-            coord_var=group.attributes[akey].value
-        else:
-            print "Warning: Unknown attribute --"+str(akey)+"-- for adios-group: "+gname
-    
 def main(argv=None):
     if argv is None:
         argv=sys.argv
