bd5b4e17e2493fba4548411fea357e481b765665
ge1 <ge1>
2008-08-12 20:30:06 +0000

    fixed up to be more clear

--- tests/C/adios_test_c.c
+++ tests/C/adios_test_c.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <assert.h>
 
 // mpi
 #include "mpi.h"
@@ -19,36 +20,39 @@ int main (int argc, char ** argv)
     int var_x1 = 101;
     int var_x2 = 102;
 
-    int z_dim_size1 = 10;
-    int z_dim_size2 = 2;
-    int z_dim_size3 = 5;
+    int zionsize1 = 10; // fixed size
+    int zionsize2 = 2;  // attr_dim
+    int zionsize3 = 5;  // attr_dim2
 
-    float * z_dim1;
-    float * z_dim2;
-    float * z_dim3;
+    float * zion1;
+    float * zion2;
+    float * zion3;
 
-    z_dim1 = malloc (z_dim_size1 * sizeof (float));
-    z_dim2 = malloc (z_dim_size1 * z_dim_size2 * sizeof (float));
-    z_dim3 = malloc (z_dim_size2 * z_dim_size3 * sizeof (float));
+    zion1 = malloc (zionsize1 * sizeof (float));
+    zion2 = malloc (zionsize2 * zionsize2 * sizeof (float));
+    zion3 = malloc (zionsize2 * zionsize3 * sizeof (float));
+    assert (zion1);
+    assert (zion2);
+    assert (zion3);
     int r_var_x1;
     int r_var_x2;
     int r_zsize;
-    float r_z [z_dim_size1];
+    float r_z [zionsize1];
 
     int node = 0;
 
     uint64_t total;
 
-    z_dim1 [0] = 10.0;
-    z_dim1 [1] = 11.0;
-    z_dim1 [2] = 12.0;
-    z_dim1 [3] = 13.0;
-    z_dim1 [4] = 14.0;
-    z_dim1 [5] = 15.0;
-    z_dim1 [6] = 16.0;
-    z_dim1 [7] = 17.0;
-    z_dim1 [8] = 18.0;
-    z_dim1 [9] = 19.0;
+    zion1 [0] = 10.0;
+    zion1 [1] = 11.0;
+    zion1 [2] = 12.0;
+    zion1 [3] = 13.0;
+    zion1 [4] = 14.0;
+    zion1 [5] = 15.0;
+    zion1 [6] = 16.0;
+    zion1 [7] = 17.0;
+    zion1 [8] = 18.0;
+    zion1 [9] = 19.0;
 
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
@@ -58,22 +62,22 @@ int main (int argc, char ** argv)
 printf ("XXXXXXXXXXXXXXXX do a write XXXXXXXXXXXXXXXXX\n");
     adios_open (&io_handle, type_name, filename, "w");
     adios_group_size (io_handle,  4 + 4
-                                + 4 * z_dim_size1
-                                + 4 + z_dim_size1 * z_dim_size2
-                                + 4 + 4 * z_dim_size2 * z_dim_size3
+                                + 4 * zionsize1
+                                + 4 + 4 * zionsize2 * zionsize2
+                                + 4 + 4 * zionsize2 * zionsize3
                                 + 4
                      ,&total, &comm
                      );
     adios_write (io_handle, "/mype", &var_x1);
     adios_write (io_handle, "/test/mype", &var_x2);
 
-    adios_write (io_handle, "zion1", z_dim1);
+    adios_write (io_handle, "zion1", zion1);
 
-    adios_write (io_handle, "zionsize2", &z_dim_size1);
-    adios_write (io_handle, "zion2", z_dim2);
+    adios_write (io_handle, "zionsize2", &zionsize2);
+    adios_write (io_handle, "zion2", zion2);
 
-    adios_write (io_handle, "zionsize3", &z_dim_size3);
-    adios_write (io_handle, "zion3", z_dim3);
+    adios_write (io_handle, "zionsize3", &zionsize3);
+    adios_write (io_handle, "zion3", zion3);
 
     adios_write (io_handle, "node-attr", &node);
 
@@ -84,7 +88,7 @@ printf ("XXXXXXXXXXXXXXXX do a write XXXXXXXXXXXXXXXXX\n");
     MPI_Barrier (MPI_COMM_WORLD);
 #endif
 
-#if 0
+#if 1
 printf ("XXXXXXXXXXXXXXXX do a read XXXXXXXXXXXXXXXXX\n");
 
     adios_open (&io_handle, type_name, filename, "r");
@@ -99,26 +103,26 @@ printf ("XXXXXXXXXXXXXXXX do a read XXXXXXXXXXXXXXXXX\n");
 
     if (   var_x1 != r_var_x1
         || var_x2 != r_var_x2
-        || r_zsize != z_dim_size
-        || r_z [0] != z_dim1 [0]
-        || r_z [1] != z_dim1 [1]
-        || r_z [2] != z_dim1 [2]
-        || r_z [3] != z_dim1 [3]
-        || r_z [4] != z_dim1 [4]
-        || r_z [5] != z_dim1 [5]
-        || r_z [6] != z_dim1 [6]
-        || r_z [7] != z_dim1 [7]
-        || r_z [8] != z_dim1 [8]
-        || r_z [9] != z_dim1 [9]
+        || r_zsize != zionsize2
+        || r_z [0] != zion1 [0]
+        || r_z [1] != zion1 [1]
+        || r_z [2] != zion1 [2]
+        || r_z [3] != zion1 [3]
+        || r_z [4] != zion1 [4]
+        || r_z [5] != zion1 [5]
+        || r_z [6] != zion1 [6]
+        || r_z [7] != zion1 [7]
+        || r_z [8] != zion1 [8]
+        || r_z [9] != zion1 [9]
        )
     {
         int i;
         printf ("rank: %d mismatch in reading\n", rank);
         printf ("r_var_x1: %d (%d)\n", r_var_x1, var_x1);
         printf ("r_var_x2: %d (%d)\n", r_var_x2, var_x2);
-        printf ("r_zsize: %d (%d)\n", r_zsize, z_dim_size);
+        printf ("r_zsize: %d (%d)\n", r_zsize, zionsize1);
         for (i = 0; i < 10; i++)
-            printf ("z_dim [%d]: %f (%f)\n", i, r_z [i], z_dim [i]);
+            printf ("z_dim [%d]: %f (%f)\n", i, r_z [i], zion1 [i]);
     }
     else
     {
@@ -126,23 +130,23 @@ printf ("XXXXXXXXXXXXXXXX do a read XXXXXXXXXXXXXXXXX\n");
     }
 #endif
 
-#if 0
+#if 1
 printf ("XXXXXXXXXXXXXXXX do an append XXXXXXXXXXXXXXXXX\n");
     var_x1 = 11;
     adios_open (&io_handle, type_name, filename, "a");
     adios_group_size (io_handle,  4 + 4
-                                + 4 * z_dim_size1
-                                + 4 + z_dim_size1 * z_dim_size2
+                                + 4 * zionsize1
+                                + 4 + 4 * zionsize2 * zionsize2
                                 + 4
                      ,&total, &comm
                      );
     adios_write (io_handle, "/mype", &var_x1);
     adios_write (io_handle, "/test/mype", &var_x2);
 
-    adios_write (io_handle, "zion1", z_dim1);
+    adios_write (io_handle, "zion1", zion1);
 
-    adios_write (io_handle, "zionsize2", &z_dim_size);
-    adios_write (io_handle, "zion2", z_dim2);
+    adios_write (io_handle, "zionsize2", &zionsize2);
+    adios_write (io_handle, "zion2", zion2);
 
     adios_write (io_handle, "node-attr", &node);
 
