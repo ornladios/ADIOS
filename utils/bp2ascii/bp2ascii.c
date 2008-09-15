207946e6981a0ab4581d36c42f538ead1995126d
Chen Jin <cgj>
2008-09-15 21:57:10 +0000

    works for scalar

--- utils/bp2ascii/bp2ascii.c
+++ utils/bp2ascii/bp2ascii.c
@@ -1,152 +1,233 @@
-#include "hw-utils.h"
-#include "hdf5.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include "adios_types.h"
+#include "adios_transport_hooks.h"
+#include "adios_bp_v1.h"
+#include "adios_internals.h"
 
-void dump2ascii(char *argv[],int argc,int i);
-double *hr_dataset(hid_t root_id, char* name,hsize_t *rank,hsize_t *dims,hid_t *type_id);
+struct dump_struct
+{
+    char * dump_var;
+    enum ADIOS_FLAG host_language_fortran;
+};
+
+struct var_dim
+{
+    uint16_t id;
+    uint64_t rank;
+};
+
+void print_process_group_header (uint64_t num
+                      ,struct adios_process_group_header_struct_v1 * pg_header
+                      );
+void print_process_group_index (
+                         struct adios_index_process_group_struct_v1 * pg_root
+                         );
+void print_var_header (struct adios_var_header_struct_v1 * var_header);
 
 int main (int argc, char ** argv)
 {
-    if (argc < 4)
+    char * filename;
+    char * var;
+    char newfilename [256];
+    int i = 0;
+    int rc = 0;
+    uint64_t element_num = 0, element_count;
+    struct adios_bp_element_struct * element = NULL;
+    struct dump_struct dump;
+
+
+    if (argc < 2)
     {
-        printf ("\nUSAGE: bp2ascii XXXX.bp  [XXXX.txt] -C[-c] data_name1  [data_name2...]\n\n");
+        fprintf (stderr, "usage: %s [-v [var]|--var [var]] <filename> [newfilename]\n"
+                ,argv [0]
+                );
+
         return -1;
     }
-    int i;
-    hw_makeh5(argv[1]);
-    for(i=0;i<argc;i++)
-      if((strcmp(argv[i],"-C")==0) || (strcmp(argv[i],"-c")==0))
-        break;
-   
-    dump2ascii(argv,argc,i+1); 
+
+    if (argv [1][0] && argv [1][0] == '-')
+    {
+        if (   !strcmp (argv [1], "-v")
+            || !strcmp (argv [1], "--var")
+           )
+        {
+            if (argc > 2)
+            {
+                dump.dump_var = argv [2];
+                filename = argv [3];
+                printf("%s %s\n",dump.dump_var,filename);
+		if (argc > 4)
+		{
+			strcpy (newfilename, argv[4]);
+		}
+		else
+		{
+			strcpy (newfilename, dump.dump_var);
+			strcat (newfilename, ".dat");
+		}
+            }
+            else
+            {
+		    fprintf (stderr, "usage: %s [-v [var]|--var [var]] <filename> [newfilename]\n"
+				    ,argv [0]
+			    );
+
+		    return -1;
+            }
+        }
+        else
+        {
+            fprintf (stderr, "usage: %s [-v [var]|--var [var]] <filename> [newfilename]\n"
+                    ,argv [0]
+                    );
+
+            return -1;
+        }
+    }    else
+    {
+        filename = argv [1];
+        dump.dump_var = 0;
+    }
+
+    struct adios_bp_buffer_struct_v1 * b = 0;
+    uint32_t version = 0;
+    FILE * outf;
+    outf = fopen (newfilename, "w");
+    b = malloc (sizeof (struct adios_bp_buffer_struct_v1));
+    adios_buffer_struct_init (b);
+
+    rc = adios_posix_open_read_internal (filename, "", b);
+    if (!rc)
+    {
+        fprintf (stderr, "bpdump: file not found: %s\n", filename);
+
+        return -1;
+    }
+
+    adios_posix_read_version (b);
+    adios_parse_version (b, &version);
+
+    struct adios_index_process_group_struct_v1 * pg_root = 0;
+    struct adios_index_process_group_struct_v1 * pg = 0;
+    struct adios_index_var_struct_v1 * vars_root = 0;
+    struct adios_index_attribute_struct_v1 * attrs_root = 0;
+
+    adios_posix_read_index_offsets (b);
+    adios_parse_index_offsets_v1 (b);
+
+    adios_posix_read_process_group_index (b);
+    adios_parse_process_group_index_v1 (b, &pg_root);
+    print_process_group_index (pg_root);
+
+    adios_posix_read_vars_index (b);
+    adios_parse_vars_index_v1 (b, &vars_root);
+
+    while (vars_root)
+    {
+        printf("%s\n",vars_root->var_name);
+        if (!strcasecmp (dump.dump_var, vars_root->var_name) )
+            break;
+        else
+            vars_root = vars_root->next;
+    }
+
+    uint64_t element_counts = vars_root->characteristics_count;
+    struct adios_process_group_header_struct_v1 pg_header;
+    struct adios_var_header_struct_v1 var_header;
+    struct adios_var_payload_struct_v1 var_payload;
+    uint64_t offset, var_len;
+    printf("characteristics count: %d\n", element_counts); 
+
+    for (i = 0; i < element_counts; i++)
+    {
+        offset = vars_root->characteristics[i].offset;
+        b->read_pg_offset = pg_root->offset_in_file;
+
+        printf("offset: %llu read_pg_offset=%llu\n", offset, b->read_pg_offset);
+        if (pg_root->next)
+        {
+            b->read_pg_size =   pg_root->next->offset_in_file
+                              - pg_root->offset_in_file;
+        }
+        else
+        {
+            b->read_pg_size =   b->pg_index_offset
+                              - pg_root->offset_in_file;
+        }
+        
+        adios_init_buffer_read_process_group (b);
+        lseek(b->f, b->read_pg_offset+offset,SEEK_SET);
+
+        read(b->f,b->buff,8);
+        var_len = *(uint64_t *) b->buff;
+        read (b->f,b->buff+8, var_len);
+        printf("var length: %llu offset: %llu\n",var_len, offset);
+        printf ("payload_size %llux\n", var_header.payload_size);
+
+        adios_parse_var_data_header_v1 (b, &var_header);
+        print_var_header (&var_header);
+        
+        var_payload.payload = malloc (var_header.payload_size);   
+	adios_parse_var_data_payload_v1 (b, &var_header, &var_payload
+                                        ,var_header.payload_size
+                                        ); 
+        fprintf(outf, "%d ", *((int *)var_payload.payload));
+        
+    }
+    fclose(outf);
+    adios_posix_close_internal (b);
     return 0;
 }
 
-void dump2ascii(char* argv[],int argc,int idx)
+void print_process_group_header (uint64_t num
+                      ,struct adios_process_group_header_struct_v1 * pg_header
+                      )
 {
-     char filename[100];
-     int len,i,j,id;
-     void *data;
-     hid_t h5file_id,root_id,type_id;
-     hsize_t rank,dims[3];
-     strcpy(filename,argv[1]);
-     len =strlen(filename);
-     filename[len-2]='h';
-     filename[len-1]='5';
-     h5file_id=H5Fopen(filename,H5F_ACC_RDWR,H5P_DEFAULT);
-     if(h5file_id<0)
-       return;
-     root_id=H5Gopen(h5file_id,"/");
-     if(idx==3)
-     { 
-     strcpy(filename,argv[1]);
-     len=strlen(argv[1]);
-     filename[len-2]='t';
-     filename[len-1]='x';
-     filename[len]='t';
-     filename[len+1]='\0';
-     }
-     if(idx==4)
-     strcpy(filename,argv[2]);
+    int i;
+    struct adios_method_info_struct_v1 * m;
 
-     printf("----------------------------\n");
-     printf("generating file: %s\n",filename);
-     FILE* fid=fopen(filename,"w");
-     for(id=idx;id<argc;id++)
-     {
-     printf("----------------------------\n");
-     printf("write dataset: %s\n",argv[id]);
-     data=hr_dataset(root_id,argv[id],&rank,dims,&type_id);
-     if(rank==1)
-     {
-        if(H5Tequal(type_id,H5Tcopy(H5T_IEEE_F64LE)))
-        {
-           for(i=0;i<dims[0];i++)
-           {
-              fprintf(fid,"%e  ",*((double *)(data+8*i)));
-              printf("\t%e  ",*((double *)(data+8*i)));
-           }
-           printf("\n");
-        }
-        else if(H5Tequal(type_id,H5Tcopy(H5T_IEEE_F32LE)))
-        {
-           {
-              for(i=0;i<dims[0];i++)
-              {
-                  fprintf(fid,"%e  ",*((float *)(data+4*i)));
-                  printf("\t%e  ",*((float *)(data+4*i)));
-              }
-              printf("\n");
-           }
-        }
-        else if(H5Tequal(type_id,H5Tcopy(H5T_STD_I32LE)))
-        {
-           for(i=0;i<dims[0];i++)
-           {
-              fprintf(fid,"%d  ",*((int*)(data+4*i)));
-              printf("\t%d  ",*((int *)(data+4*i)));
-           }
-           fprintf(fid,"%d  ",*((int*)(data+4*i)));
-           printf("\n");
-        }
-            
-     }
-     if(rank==2)
-     {
-        if(H5Tequal(type_id,H5Tcopy(H5T_IEEE_F32LE)))
-              for(i=0;i<dims[0];i++)
-              {
-                  for(j=0;j<dims[1];j++)
-                      fprintf(fid,"%e  ",*((float*)(data+i*dims[1]+4*j)));
-                  fprintf(fid,"\n"); 
-              }
-        else if(H5Tequal(type_id,H5Tcopy(H5T_IEEE_F64LE)))
-              for(i=0;i<dims[0];i++)
-              {
-                  for(j=0;j<dims[1];j++)
-                      fprintf(fid,"%e  ",*((double*)(data+i*dims[1]+8*j)));
-                  fprintf(fid,"\n"); 
-              }
-        else if(H5Tequal(type_id,H5Tcopy(H5T_STD_I32LE)))
-              for(i=0;i<dims[0];i++)
-              {
-                  for(j=0;j<dims[1];j++)
-                      fprintf(fid,"%e  ",*((int*)(data+i*dims[1]+4*j)));
-                  fprintf(fid,"\n"); 
-              }
-        else if(H5Tequal(type_id,H5Tcopy(H5T_STD_I64LE)))
-              for(i=0;i<dims[0];i++)
-              {
-                  for(j=0;j<dims[1];j++)
-                      fprintf(fid,"%e  ",*((long *)(data+i*dims[1]+8*j)));
-                  fprintf(fid,"\n"); 
-              }
-     }
-     }
-     fclose(fid);
+    printf ("Process Group: %llu\n", num);
+    printf ("\tGroup Name: %s\n", pg_header->name);
+    printf ("\tHost Language Fortran?: %c\n"
+           ,(pg_header->host_language_fortran == adios_flag_yes ? 'Y' : 'N')
+           );
+    printf ("\tCoordination Var Member ID: %d\n", pg_header->coord_var_id);
+    printf ("\tTime Name: %s\n", pg_header->time_index_name);
+    printf ("\tTime: %d\n", pg_header->time_index);
+    printf ("\tMethods used in output: %d\n", pg_header->methods_count);
+    m = pg_header->methods;
+    while (m)
+    {
+        printf ("\t\tMethod ID: %d\n", m->id);
+        printf ("\t\tMethod Parameters: %s\n", m->parameters);
+
+        m = m->next;
+    }
 }
-void *hr_dataset(hid_t root_id, char* name,hsize_t *rank,hsize_t *dims,hid_t *type_id)
+
+void print_process_group_index (
+                         struct adios_index_process_group_struct_v1 * pg_root
+                         )
 {
-   int i,j,size;
-   void *temp;
-   hid_t dsethist_id,filespace_id;
-   //type_id = H5Tcopy(H5T_IEEE_F64LE);
-   dsethist_id=H5Dopen(root_id,name);
-   *type_id=H5Dget_type(dsethist_id);
-   size=H5Tget_size(*type_id);  
-   filespace_id=H5Dget_space(dsethist_id);
-   *rank=H5Sget_simple_extent_ndims(filespace_id);
-   H5Sget_simple_extent_dims(filespace_id,dims,NULL);
-   if (*rank==1)
-   {
-      printf("\tDimension:%d \n",dims[0]);
-      temp= malloc(size*dims[0]);
-      H5Dread(dsethist_id,*type_id,H5S_ALL,H5S_ALL,H5P_DEFAULT,temp);
-   }
-   else if (*rank==2)
-   {
-      printf("\tDimension:%d x %d \n",dims[0],dims[1]);
-      temp=malloc(size*dims[0]*dims[1]);
-      H5Dread(dsethist_id,*type_id,H5S_ALL,H5S_ALL,H5P_DEFAULT,temp);
-   }
-  return  temp;
+    while (pg_root)
+    {
+        printf ("Group: %s\n", pg_root->group_name);
+        printf ("\tProcess ID: %d\n", pg_root->process_id);
+        printf ("\tTime Name: %s\n", pg_root->time_index_name);
+        printf ("\tTime: %d\n", pg_root->time_index);
+        printf ("\tOffset in File: %llu\n", pg_root->offset_in_file);
+
+        pg_root = pg_root->next;
+    }
+}
+void print_var_header (struct adios_var_header_struct_v1 * var_header)
+{
+    int i = 0;
+    printf ("\t\tVar Name (ID): %s (%d)\n", var_header->name, var_header->id);
+    printf ("\t\tVar Path: %s\n", var_header->path);
+    printf ("\t\tDatatype: %s\n", adios_type_to_string (var_header->type));
+    printf ("\t\tIs Dimension: %c\n"
+           ,(var_header->is_dim == adios_flag_yes ? 'Y' : 'N')
+           );
 }
