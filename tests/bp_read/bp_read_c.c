032191b704903dc4507fb71e479296b07c67f6b2
Chen Jin <cgj>
2009-05-24 08:08:03 +0000

    add c and fortran routines for bp read apis

---
+++ tests/bp_read/bp_read_c.c
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include "mpi.h"
+#include "bp_read.h"
+
+int main (int argc, char ** argv)
+{
+	char * filename;
+	int    rank, pe_size, rc, version;	
+	struct bp_index_pg_struct_v1 * pg_root = 0;
+	struct bp_index_pg_struct_v1 * pg = 0;
+
+	struct  adios_index_var_struct_v1 * vars_root = 0;
+	struct  adios_index_attribute_struct_v1 * attrs_root = 0;
+	int64_t fh=0, gh=0;
+	int     ngroup, nvar, nattr, ntstep;
+	int     i,j,k;	
+
+	int	NX = 10, G_NX=30;
+	int	NY = 10, G_NY=30;
+	int	NZ = 10, G_NZ=30;
+	int  	dims [10];
+	void    * var = NULL;
+	MPI_Comm comm; 
+
+	filename = argv[1];
+	comm = MPI_COMM_WORLD;
+	MPI_Init (&argc, &argv);
+	MPI_Comm_rank (comm, &rank);
+	MPI_Comm_size (comm, &pe_size);
+	bp_fopen (&fh, filename, comm);
+    printf("open!\n");
+	// fh = file handle, filename = name of file, comm is the 
+	// communicator for the processors which will read in the data.
+	// in this call, the header is actually being read, and this is done by proc 0, and
+	// then it is broadcast to all of the processors.
+	BP_FILE_INFO finfo;
+	/*
+	   BP_FILE_INFO's memmbers:
+		uint16_t namelist_true;
+		uint16_t groups_count;
+		uint16_t vars_count;
+		uint16_t attrs_count;
+		uint32_t tidx_start;
+		uint32_t tidx_stop;
+		uint32_t version;
+		uint32_t file_size;
+		char     ** group_namelist;
+	 */
+	bp_init_fileinfo ( &finfo, 1);
+	// initialize the FILE_INFO structure
+	//       IN: 
+	// 	    struct BP_FILE_INFO * 
+ 	// 	    int flag: 1 means allocate the momory for group_namelist
+        //		      0 means no allocation 
+	// * bp_free_fileinfo (&finfo) need to be paired with this function call
+	bp_inq_file (fh, &finfo);
+	
+	if (rank == 0) 
+		bp_print_fileinfo (&finfo);
+
+	BP_GROUP_INFO ginfo;
+	bp_gopen (&gh, fh, finfo.group_namelist[0]);
+	bp_init_groupinfo (&ginfo,1);
+	bp_inq_group (gh, &ginfo);
+	if (rank == 0) 
+		bp_print_groupinfo (&ginfo);
+
+	int type, ndim, time_flag;
+	int start[3], size[3];
+	char *type_str;
+    double time;
+    // get scalar data
+	bp_inq_var (gh, "int_1D", &type, &ndim, &time_flag, dims);
+	type_str = bp_type_to_string (type);
+    printf("%s:\n\t\tis_timebased: %d\n\t\ttype: %s\n",
+		    "int_1D", time_flag, type_str);
+    if (ndim==0) 
+        printf("\t\tscalar\n");
+    else 
+            printf("\t\tdimension: [%d]", dims[0]);
+    size[0] = 1;
+    start[0] = 0;
+	bp_get_var (gh, "int_1D", &nvar, start, size, 1);
+    printf("\t\tvalue: %d\n",(nvar));
+    // get 2D data
+	bp_inq_var (gh, "int_2D", &type, &ndim, &time_flag, dims);
+    printf("%s:\n\t\tis_timebased: %d\n\t\ttype: %s\n\t\tdimensions:",
+    		"int_2D", time_flag, type_str);
+    if (ndim==0) {
+        printf("\t\tscalar\n");
+    }
+    else {
+        for (i=0;i<ndim;i++) {
+            printf(" [%d]", dims[i]); 
+        }
+	}
+	printf("\n");
+	start[0]=0;
+	start[1]=0;
+	size[0]=10;
+	size[1]=2;
+	var = malloc (sizeof(int) * size[0]*size[1]);
+
+	// time step should be no less than zero
+	// vnamelist[14] is not written at time step 0
+	// so the function returns as error
+	bp_get_var (gh, "int_2D", var, start, size, 1);
+	k=0;
+    printf("\t\t[%d:%d, %d:%d]", 
+           start[0], size[0],
+           start[1], size[1]
+           );
+    for (j=0;j<size[0];j++) {
+        printf("\n\t\t");
+        for (i=0;i<size[1];i++){
+            printf("%d  ",*(int*)(var+4*k));
+            k++;
+        }
+    }
+	printf("\n");
+
+	bp_inq_var (gh, "int_3D", &type, &ndim, &time_flag, dims);
+    printf("%s:\n\t\tis_timebased: %d\n\t\ttype: %s\n\t\tdimensions:",
+    		"int_3D", time_flag, type_str);
+    if (ndim==0) {
+        printf("\t\tscalar\n");
+    }
+    else {
+        for (i=0;i<ndim;i++) {
+            printf(" [%d]", dims[i]); 
+        }
+	    printf("\n");
+	}
+    
+    if (var)
+        free (var);
+	var = malloc ( sizeof(int) * dims[0]*dims[1]*dims[2]);
+	start[0]=0;
+	start[1]=0;
+	start[2]=0;
+	size[0]=2;
+	size[1]=5;
+	size[2]=3;
+	bp_get_var (gh, "int_3D", var, start, size, 1);
+	k=0;
+    printf("\t\t[%d:%d, %d:%d, %d:%d]", 
+           start[0], size[0],
+           start[1], size[1],
+           start[2], size[2]
+           );
+    for (nvar=0;nvar<size[0];nvar++) {
+        for (j=0;j<size[1];j++) {
+            printf("\n\t\t");
+            for (i=0;i<size[2];i++){
+                printf("%d  ",*(int*)(var+4*k));
+                k++;
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\ndone\n");
+
+    if (var)
+		free(var);
+        
+	bp_free_groupinfo (&ginfo);
+	bp_free_fileinfo (&finfo);
+
+	bp_gclose (gh);
+	bp_fclose (fh);
+	MPI_Finalize ();
+
+	return 0;
+}
+
---
+++ tests/bp_read/bp_read_f.F90
@@ -0,0 +1,133 @@
+program read_bp_f
+    implicit none
+    include "mpif.h"
+
+    integer :: gcnt, vcnt, acnt, tstart, tstop, vrank, vtype, vtimed, ierr
+    integer :: comm,i,j,k,l,m
+    integer*8 :: fh, gh
+    integer, dimension(10) :: dims, start, readsize
+    integer, dimension(1000) :: var, vartrue
+    character (len=100), dimension(50) :: vnamelist
+    character (len=100), dimension(50) :: gnamelist
+    
+    call MPI_Init (ierr)
+    comm = MPI_COMM_WORLD
+
+    call bp_fopen (fh, "testbp_c.bp"//char(0), comm, ierr)
+    call bp_inq_file (fh,gcnt,vcnt,acnt,tstart,tstop,gnamelist,ierr) 
+
+    call bp_gopen (gh, fh, gnamelist(1), ierr) 
+    call bp_inq_group(gh, vcnt, vnamelist, ierr)
+
+    do i=1,vcnt 
+        write (0,"i3, a, a") i,")  ", vnamelist(i)
+    enddo
+    ! vtype 
+    ! 0 byte 
+    ! 1 short 
+    ! 2 integer
+    ! 4 long 
+    ! 5 real 
+    ! 6 double
+    ! 7 long double
+    ! 9 string
+    ! 10 complex
+    ! 11 double_complex
+    ! 50 unsigned_byte 
+    ! 51 unsigned_short
+    ! 52 unsigned_integer
+    ! 54 unsigned_long
+    #if 0
+    write (*,*) "name    ",  "  ndim    ", "    timed"
+    call bp_inq_var (gh, vnamelist(1),vtype, vrank, vtimed, dims, ierr)
+    write (*,*)"-----------------------------"
+    write (*,"a10 i3 i3") vnamelist(1),vrank, vtimed
+    call bp_inq_var (gh, vnamelist(2),vtype, vrank, vtimed, dims, ierr)
+    write (*,*)"-----------------------------"
+    write (*,"a10 i3 i3") vnamelist(2),vrank, vtimed
+    call bp_inq_var (gh, vnamelist(3),vtype, vrank, vtimed, dims, ierr)
+    write (*,*)"-----------------------------"
+    write (*,"a10 i3 i3") vnamelist(3),vrank, vtimed
+    start(1:10)=0
+    readsize(1:10)=1 
+    ! -------------- read 1D data ------------------
+    call bp_inq_var (gh, "int_1D"//char(0), vtype, vrank, vtimed, dims, ierr)
+    readsize(1)=dims(1)
+    call bp_get_var (gh, "int_1D"//char(0), var, start, readsize, 1, ierr)
+    write (*,*)"-----------------------------"
+    write (*,"a6 i3") "int_1D", dims(1)
+    write (*, "10i3") var(1:readsize(1))
+    ! -------------- read 2D data ------------------
+    call bp_inq_var (gh, "int_2D"//char(0),vtype, vrank, vtimed, dims, ierr)
+    call bp_get_var (gh, "int_2D"//char(0), var, start, readsize, 1, ierr)
+    write (*,*)"-----------------------------"
+    write (*,"a6 a3 (2i2) a3 (2i4) a1") "int_2D","[",dims(1:2),"] [",readsize(1:2), "]"
+    write (*,"10i3") var(1:readsize(1)*readsize(2))
+    ! -------------- read 3D data ------------------
+    call bp_inq_var (gh,"int_3D"//char(0),vtype, vrank, vtimed, dims, ierr)
+    start(1:10)=0
+    readsize(1)=dims(1) 
+    readsize(2:10)=1
+    readsize(2)=1
+    call bp_get_var (gh, "int_3D"//char(0), var, start, readsize, 1, ierr)
+    write (*,*)"-----------------------------"
+    write (*,"a6 a3 (3i3) a3 (3i3) a1") "int_3D",'[',dims(1:3),'] [',readsize(1:3),"]"
+    write (*,"20i4") var(1:readsize(1)*readsize(2)*readsize(3))
+    #endif
+
+    ! -------------- read 4D data ------------------
+    call bp_inq_var (gh, "int_4D"//char(0),vtype, vrank, vtimed, dims, ierr)
+    readsize(1)=dims(1) 
+    readsize(2)=1
+    readsize(3)=1
+    readsize(4)=2
+    call bp_get_var (gh, "int_4D"//char(0), var, start, readsize, 1, ierr)
+    write (*,*)"-----------------------------"
+    write (*,"a6 a2 (4i3) a3 (4i3) a1") "int_4D","[",dims(1:4),"] [",readsize(1:4),"]" 
+    write (*,"80i5") var(1:readsize(1)*readsize(2)*readsize(3)*readsize(4))
+    vcnt = 1
+    do i=0,readsize(1)-1
+    do j=0,readsize(2)-1
+    do k=0,readsize(3)-1
+    do l=0,readsize(4)-1
+        acnt = ((i*dims(2)+j)*dims(3)+k)*dims(4)+l
+        vartrue(vcnt)=acnt
+        vcnt = vcnt + 1
+    enddo
+    enddo
+    enddo
+    enddo
+    write (*,"80i5") vartrue(1:readsize(1)*readsize(2)*readsize(3)*readsize(4))
+
+    #if 1
+    ! -------------- read 5D data ------------------
+    call bp_inq_var (gh, "int_5D"//char(0),vtype, vrank, vtimed, dims, ierr)
+    readsize(1)=dims(1)
+    readsize(2:10)=1
+    readsize(3)=dims(3)
+
+    vcnt = 1
+    do i=0,readsize(1)-1
+    do j=0,readsize(2)-1
+    do k=0,readsize(3)-1
+    do l=0,readsize(4)-1
+    do m=0,readsize(5)-1
+        acnt = (((i*dims(2)+j)*dims(3)+k)*dims(4)+l)*dims(5)+m
+        vartrue(vcnt)=acnt
+        vcnt = vcnt + 1
+    enddo
+    enddo
+    enddo
+    enddo
+    enddo
+    write (*,*)"-----------------------------"
+    write (*,"a a2 (5i3) a3 (5i3) a1") "int_5D","[",dims(1:5),"] [",readsize(1:5),"]" 
+    call bp_get_var (gh, "int_5D"//char(0), var, start, readsize, 1, ierr)
+    write (*,"100i5")var(1:readsize(1)*readsize(2)*readsize(3)*readsize(4)*readsize(5))
+    write (*,"100i5")vartrue(1:readsize(1)*readsize(2)*readsize(3)*readsize(4)*readsize(5))
+    #endif
+    call bp_gclose(gh, ierr)
+    call bp_fclose(fh, ierr)
+
+    call MPI_Finalize (ierr)
+end program
