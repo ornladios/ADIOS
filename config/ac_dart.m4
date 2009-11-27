8fc3927f51864dd43c01d322777100a38c2df628
Norbert Podhorszki <pnorbert@github.com>
2009-11-27 21:17:05 +0000

    Added --with-dart=DIR configure option and added adios-dart.c method to the adios lib if dart is specified.

---
+++ config/ac_dart.m4
@@ -0,0 +1,116 @@
+#
+#
+# AC_PROG_DART
+#
+# Test for DART installation
+# and set $DART to the correct value.
+#
+#
+dnl @synopsis AC_PROG_DART
+dnl
+dnl This macro test if DART is installed. If DART
+dnl is installed, it set $DART to the right value
+dnl
+dnl @version 1.0
+dnl @author Norbert Podhorszki, pnorbert@ornl.gov
+dnl
+AC_DEFUN([AC_PROG_DART],[
+
+AM_CONDITIONAL(HAVE_DART,true)
+
+AC_ARG_WITH(dart,
+        [AS_HELP_STRING([--with-dart=DIR],
+           [Build the DART transport method. Point to the DART installation.])],
+        [DART_LDFLAGS="-L$withval/lib";
+         DART_CPPFLAGS="-I$withval/include";],
+        [with_dart=check])
+
+dnl If --without-dart was given set HAVE_DART to false and do nothing more
+if test "x$with_dart" == "xno"; then
+
+   AM_CONDITIONAL(HAVE_DART,false)
+
+else
+
+    dnl allow args --with-dart incdir and --with-dart-libdir
+    AC_ARG_WITH(dart-incdir,
+                [  --with-dart-incdir=<location of dart includes>],
+                [DART_INCDIR=$withval
+                 with_dart=detailed])
+    
+    AC_ARG_WITH(dart-libdir,
+                [  --with-dart-libdir=<location of dart library>],
+                [DART_LIBDIR=$withval
+                 with_dart=detailed])
+    
+    
+    dnl If we know DART_DIR, then we can know DART_INCDIR.
+    dnl We don't overwrite DART_INCDIR.
+    if test -n "${DART_DIR}" -a -z "${DART_INCDIR}"; then
+            DART_INCDIR="${DART_DIR}/include";
+    dnl We may have DART denoting the dir (e.g. on ewok BUT on franklin it contains all flags)
+    elif test -n "${DART}" -a -d "${DART}"; then
+            DART_INCDIR="${DART}/include"
+    fi
+
+    dnl If we know DART_DIR, then we can know DART_LIBDIR.
+    dnl We don't overwrite DART_LIBDIR.
+    if test -n "${DART_DIR}" -a -z "${DART_LIBDIR}"; then
+            DART_LIBDIR="${DART_DIR}/lib";
+    dnl We may have DART denoting the dir (e.g. on ewok BUT on franklin it contains all flags)
+    elif test -n "${DART}" -a -d "${DART}"; then
+            DART_LIBDIR="${DART}/lib"
+    fi
+
+    dnl Add "-I" to DART_INCDIR.
+    if test -n "${DART_INCDIR}"; then
+            DART_CPPFLAGS="-I${DART_INCDIR}"
+    else
+            ac_dart_ok=no
+    fi
+
+    dnl Add "-L" to DART_LIBDIR.
+    if test -n "${DART_LIBDIR}"; then
+            DART_LDFLAGS="-L${DART_LIBDIR}"
+    else
+            ac_dart_ok=no
+    fi
+
+    save_CPPFLAGS="$CPPFLAGS"
+    save_LIBS="$LIBS"
+    save_LDFLAGS="$LDFLAGS"
+    LIBS="$LIBS -ldart2"
+    LDFLAGS="$LDFLAGS $DART_LDFLAGS"
+    CPPFLAGS="$CPPFLAGS $DART_CPPFLAGS"
+    
+    if test -z "${HAVE_DART_TRUE}"; then
+            AC_CHECK_HEADERS(dc_gspace.h,
+                    ,
+                    [AM_CONDITIONAL(HAVE_DART,false)])
+    fi
+    
+    # Check for the Mini-XML library and headers
+    AC_TRY_COMPILE([#include "dc_gspace.h"],
+            [struct dcg_space *dcg; dcg = dcg_alloc(1,1);],
+            [DART_LIBS="-ldart2"],
+            [AM_CONDITIONAL(HAVE_DART,false)])
+    
+    LIBS="$save_LIBS"
+    LDFLAGS="$save_LDFLAGS"
+    CPPFLAGS="$save_CPPFLAGS"
+    
+    AC_SUBST(DART_LIBS)
+    AC_SUBST(DART_LDFLAGS)
+    AC_SUBST(DART_CPPFLAGS)
+    
+    # Finally, execute ACTION-IF-FOUND/ACTION-IF-NOT-FOUND:
+    if test -z "${HAVE_DART_TRUE}"; then
+            ifelse([$1],,[AC_DEFINE(HAVE_DART,1,[Define if you have the DART.])],[$1])
+            :
+    else
+            $2
+            :
+    fi
+
+fi
+])dnl AC_DART
--- src/adios_dart.c
+++ src/adios_dart.c
@@ -15,10 +15,7 @@
 #include <mxml.h>
 
 // dart
-#include <sys/uio.h>
-#if USE_PORTALS
-#include "sender.h"
-#endif
+#include "dc_gspace.h"
 
 #include "adios.h"
 #include "adios_transport_hooks.h"
@@ -28,27 +25,45 @@ static int adios_dart_initialized = 0;
 
 struct adios_DART_data_struct
 {
-    struct iovec ioarr;
+    struct dcg_space *dcg;
+    int version;            /* Versioning of objects put in space */
 };
 
-#if USE_PORTALS
 void adios_dart_init (const char * parameters
                      ,struct adios_method_struct * method
                      )
 {
+    struct adios_DART_data_struct * p = 0;
     if (!adios_dart_initialized)
     {
         adios_dart_initialized = 1;
-
-        init (DEFAULT_CONF);
     }
     method->method_data = calloc (1, sizeof (struct adios_DART_data_struct));
+    p = (struct adios_DART_data_struct *) method->method_data;
+    p->dcg = 0;
+    p->version = 0;
 }
 
 int adios_dart_open (struct adios_file_struct * fd
                     ,struct adios_method_struct * method, void * comm
                     )
 {
+    struct adios_DART_data_struct * p = (struct adios_DART_data_struct *)
+                                                              method->method_data;
+    switch (fd->mode) 
+    {
+        case adios_mode_read:
+        {
+            fprintf (stderr, "ADIOS DART: Read mode is not supported\n");
+        }
+
+        case adios_mode_write:
+        case adios_mode_append:
+        {
+        }
+    } // switch (fd->mode)
+
+
     return 1;
 }
 
@@ -70,11 +85,6 @@ void adios_dart_write (struct adios_file_struct * fd
     struct adios_DART_data_struct * d =
                   (struct adios_DART_data_struct *) method->method_data;
 
-    d->ioarr.iov_base = buffer + start;
-    d->ioarr.iov_len = end - start;
-
-    ptl_cn_send_sync (&d->ioarr, count, dst_tcp);
-//    ptl_cn_send (&d->ioarr, count, dst_tcp);
 }
 
 void adios_dart_get_write_buffer (struct adios_file_struct * fd
@@ -120,92 +130,3 @@ void adios_dart_start_calculation (struct adios_method_struct * method)
 void adios_dart_stop_calculation (struct adios_method_struct * method)
 {
 }
-#else
-void adios_dart_init (const char * parameters
-                     ,struct adios_method_struct * method
-                     )
-{
-    fprintf (stderr, "adios_dart_init: DART disabled, no portals support\n");
-    if (!adios_dart_initialized)
-    {
-        adios_dart_initialized = 1;
-    }
-    method->method_data = 0;
-}
-
-int adios_dart_open (struct adios_file_struct * fd
-                    ,struct adios_method_struct * method, void * comm
-                    )
-{
-    fprintf (stderr, "adios_dart_open: DART disabled, no portals support\n");
-
-    return 1;
-}
-
-enum ADIOS_FLAG adios_dart_should_buffer (struct adios_file_struct * fd
-                                         ,struct adios_method_struct * method
-                                         )
-{
-    return adios_flag_no;   // we'll buffer
-}
-
-void adios_dart_write (struct adios_file_struct * fd
-                      ,struct adios_var_struct * v
-                      ,void * data
-                      ,struct adios_method_struct * method
-                      )
-{
-    fprintf (stderr, "adios_dart_write: DART disabled, no portals support\n");
-}
-
-void adios_dart_get_write_buffer (struct adios_file_struct * fd
-                                 ,struct adios_var_struct * v
-                                 ,uint64_t * size
-                                 ,void ** buffer
-                                 ,struct adios_method_struct * method
-                                 )
-{
-    fprintf (stderr, "adios_dart_get_write_buffer: DART disabled, "
-                     "no portals support\n"
-            );
-}
-
-void adios_dart_read (struct adios_file_struct * fd
-                     ,struct adios_var_struct * v, void * buffer
-                     ,uint64_t buffer_size
-                     ,struct adios_method_struct * method
-                     )
-{
-    fprintf (stderr, "adios_dart_read: DART disabled, no portals support\n");
-}
-
-void adios_dart_close (struct adios_file_struct * fd
-                      ,struct adios_method_struct * method
-                      )
-{
-    fprintf (stderr, "adios_dart_close: DART disabled, no portals support\n");
-}
-
-void adios_dart_finalize (int mype, struct adios_method_struct * method)
-{
-    if (adios_dart_initialized)
-    {
-        adios_dart_initialized = 0;
-    }
-    fprintf (stderr, "adios_dart_finalize: "
-                     "DART disabled, no portals support\n"
-            );
-}
-
-void adios_dart_end_iteration (struct adios_method_struct * method)
-{
-}
-
-void adios_dart_start_calculation (struct adios_method_struct * method)
-{
-}
-
-void adios_dart_stop_calculation (struct adios_method_struct * method)
-{
-}
-#endif
