d755a2de139f6136d16f475dc8c1f013d5de24df
Norbert Podhorszki <pnorbert@github.com>
2010-04-14 19:48:04 +0000

    All the changes so far commited...

--- src/read_dart.c
+++ src/read_dart.c
@@ -12,6 +12,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+//#include <errno.h>  /* ENOMEM */
 #include "adios.h"
 #include "bp_utils.h"
 #include "bp_types.h"
@@ -20,9 +21,9 @@
 #include "adios_read_hooks.h"
 #include "adios_error.h"
 #include "futils.h"
+#include "globals.h"
 
-#include "dc_gspace.h"
-#include "ss_data.h"
+#include "dart.h"
 
 #ifdef DMALLOC
 #include "dmalloc.h"
@@ -36,12 +37,11 @@
 
 #define MAXDARTNAMELEN 128
 
-// DART needs an application ID. Suppose 1 is the writer, so we elect to be 2.
-static const int DART_APPLICATION_ID = 2; 
-static struct dcg_space *dcg = 0;
-static enum storage_type st = column_major;
+/*#define DART_DO_VERSIONING   define it at configure as -DDART_DO_VERSIONING in CFLAGS */
+static int number_of_fopens = 0;  /* for versioning, works only if one file is fopened (in a loop) in the application */
 
 struct adios_read_dart_data_struct {
+    char *fname;               // path of file 
     int access_version;        // counting the access
     int disconnect_at_fclose;  // disconnect from DART in fclose()
     int mpi_rank;              // just for debug prints
@@ -58,36 +58,41 @@ static int adios_read_dart_get (const char * varname, enum ADIOS_DATATYPES varty
 */
 int adios_read_dart_init (MPI_Comm comm) 
 { 
-    int  nproc, dart_rank, dart_peers;
-    int  rank;
+    int  nproc, drank, dpeers;
+    int  rank, err;
+    int  appid, was_set;
     MPI_Comm_rank(comm, &rank);
     MPI_Comm_size(comm, &nproc);
 
-    /* Fortran call dart_init (nproc, 2): */
-    DBG_PRINTF("-- %s, rank %d: connect to dart with nproc=%d and appid=%d\n", __func__, rank, nproc, DART_APPLICATION_ID);
-    dcg = dcg_alloc(nproc, DART_APPLICATION_ID);
-    if (!dcg) {
-        error(err_connection_failed, "Failed to connect with DART\n");
-        return -err_connection_failed;
+    /* Connect to DART, but only if we are not yet connected (from Write API) */
+    if (!globals_adios_is_dart_connected()) {
+        appid = globals_adios_get_application_id (&was_set);
+        if (!was_set) 
+            appid = 2;
+        DBG_PRINTF("-- %s, rank %d: connect to dart with nproc=%d and appid=%d\n", __func__, rank, nproc, appid);
+        err = dart_init(nproc, appid);
+        if (err < 0) {
+            error(err_connection_failed, "Failed to connect with DART\n");
+            return -err_connection_failed;
+        }
+
+        //drank = dart_rank(dcg);
+        //dpeers = dart_peers(dcg);
     }
-
-    /* Fortran call dart_rank(dartrank): */
-    dart_rank = dcg_get_rank(dcg);
-    /* Fortran call dart_peers(dartpeers) */
-    dart_peers = dcg_get_num_peers(dcg);
-
+    globals_adios_set_dart_connected_from_reader();
     return 0; 
 }
 
 int adios_read_dart_finalize () 
 { 
-    if (dcg) {
-        /* Fortran call dart_finalize */
-        dcg_free(dcg);
-        dcg = NULL;
+    // disconnect from DART only if we the reader is connected (the writer not anymore)
+    if (globals_adios_is_dart_connected_from_reader() && 
+        !globals_adios_is_dart_connected_from_both()) 
+    {
+        dart_finalize();
+        DBG_PRINTF("-- %s: disconnected from dart\n", __func__);
     }
-    DBG_PRINTF("-- %s: disconnect from dart\n", __func__);
-    return 0; 
+    globals_adios_set_dart_disconnected_from_reader();
 }
 
 ADIOS_FILE * adios_read_dart_fopen (const char * fname, MPI_Comm comm)
@@ -105,11 +110,16 @@ ADIOS_FILE * adios_read_dart_fopen (const char * fname, MPI_Comm comm)
     }
 
     /* fill out dart method specific struct */
-    ds->access_version = 0;
+    ds->fname = strdup(fname);
+#ifdef DART_DO_VERSIONING
+    ds->access_version = number_of_fopens;  /* Read data of separate versions from DataSpaces */
+#else
+    ds->access_version = 0;    /* Data in DataSpaces is always overwritten (read same version) */
+#endif
     MPI_Comm_rank(comm, &ds->mpi_rank);
 
     /* if not connected to DART, connect now (and disconnect in fclose) */
-    if (!dcg) {
+    if (!globals_adios_is_dart_connected_from_reader()) {
         DBG_PRINTF("-- %s, rank %d: call init first\n", __func__, ds->mpi_rank);
         if (!adios_read_dart_init(comm)) {
             return NULL;
@@ -128,14 +138,16 @@ ADIOS_FILE * adios_read_dart_fopen (const char * fname, MPI_Comm comm)
     char dart_fname[MAXDARTNAMELEN];
     snprintf(dart_fname, MAXDARTNAMELEN, "FILE@%s",fname);
     DBG_PRINTF("-- %s, rank %d: Get variable %s\n", __func__, ds->mpi_rank, dart_fname);
-    DBG_PRINTF("   rank %d: call dcg_lock_on_read()\n", ds->mpi_rank);
-    dcg_lock_on_read();
+    /* We perform dart_lock_on_read here and release it in fclose */
+    DBG_PRINTF("   rank %d: call dcg_lock_on_read(%s)\n", ds->mpi_rank, fname);
+    dart_lock_on_read(fname);
     DBG_PRINTF("   rank %d: dart_get %s\n", ds->mpi_rank, dart_fname);
     err = adios_read_dart_get(dart_fname, time_index_type, ds, offset, readsize, &time_index);
-    DBG_PRINTF("   rank %d: call dcg_unlock_on_read()\n", ds->mpi_rank);
-    dcg_unlock_on_read();
     if (err) {
         error(err_file_not_found_error, "Data of '%s' does not exist in DataSpaces\n", dart_fname);
+        DBG_PRINTF("   rank %d: call dcg_unlock_on_read(%s)\n", ds->mpi_rank, fname);
+        dart_unlock_on_read(fname);
+        free(ds);
         return NULL;
     } else {
         DBG_PRINTF("-- %s, rank %d: data of '%s' exists, time index = %d\n", __func__, ds->mpi_rank, dart_fname, time_index);
@@ -170,6 +182,9 @@ ADIOS_FILE * adios_read_dart_fopen (const char * fname, MPI_Comm comm)
         }
     }
     DBG_PRINTF("-- %s, rank %d: done fp=%x, fp->fh=%x\n", __func__, ds->mpi_rank, fp, fp->fh);
+#ifdef DART_DO_VERSIONING
+    number_of_fopens++;
+#endif
     return fp;
 }
 
@@ -181,12 +196,17 @@ int adios_read_dart_fclose (ADIOS_FILE *fp)
     adios_errno = 0;
 
     DBG_PRINTF("-- %s, rank %d: fp=%x\n", __func__, ds->mpi_rank, fp);
+
+    /* Release read lock locked in fopen */
+    dart_unlock_on_read(ds->fname);
+    
     /* Disconnect from DART if we connected in fopen() */
     if (ds && ds->disconnect_at_fclose) {
         adios_read_dart_finalize();
     }
 
     free_namelist ((fp->group_namelist),fp->groups_count);
+    if (ds->fname) { free(ds->fname); ds->fname = 0; }
     free(ds);
     free(fp);
     return 0;
@@ -195,7 +215,7 @@ int adios_read_dart_fclose (ADIOS_FILE *fp)
 /* This function can be called if user places 
    the wrong sequences of dims for a var 
 */
-void adios_read_bp_reset_dimension_order (ADIOS_FILE *fp, int is_fortran)
+void adios_read_dart_reset_dimension_order (ADIOS_FILE *fp, int is_fortran)
 {
     /* unimplemented */
 }
@@ -213,7 +233,6 @@ ADIOS_GROUP * adios_read_dart_gopen_byid (ADIOS_FILE *fp, int grpid)
     ADIOS_GROUP * gp;
 
     /* DART has no groups, so any grpid is accepted and the same empty stuff is returned */
-    /* But we perform dart_lock_on_read here and release it in gclose */
 
     adios_errno = 0;
     gp = (ADIOS_GROUP *) malloc(sizeof(ADIOS_GROUP));
@@ -231,10 +250,6 @@ ADIOS_GROUP * adios_read_dart_gopen_byid (ADIOS_FILE *fp, int grpid)
     gp->var_namelist = 0;
     gp->attr_namelist = 0;
     
-    DBG_PRINTF("-- %s, rank %d: call dcg_lock_on_read() gp=%x fp=%x\n", __func__, ds->mpi_rank, gp, gp->fp);
-    dcg_lock_on_read();
-    DBG_PRINTF("-- %s, rank %d: returned from dcg_lock_on_read()\n", __func__);
-
     return gp;
 }
                    
@@ -243,8 +258,6 @@ int adios_read_dart_gclose (ADIOS_GROUP *gp)
     struct adios_read_dart_data_struct * ds = (struct adios_read_dart_data_struct *) gp->fp->fh;
 
     adios_errno = 0;
-    /* Release read lock locked in gopen */
-    dcg_unlock_on_read();
 
     free_namelist ((gp->var_namelist),gp->vars_count);
     free_namelist ((gp->attr_namelist),gp->attrs_count);
@@ -328,39 +341,28 @@ static int adios_read_dart_get (const char * varname, enum ADIOS_DATATYPES varty
                                 struct adios_read_dart_data_struct * ds, 
                                 int * offset, int * readsize, void * data)
 {
+
     struct obj_data *od;
     int elemsize = common_read_type_size(vartype, NULL);
     int err;
-    /* Fortran call dart_get( name, version, {bounding box}, data) */
-    struct obj_descriptor odsc = {
-        .version = ds->access_version, 
-        .owner = -1,
-        .size = elemsize,
-        .st = st, // column_major,
-        .bb = {
-            .num_dims = 3,
-            .lb.c = {offset[1], offset[0], offset[2]}, // !! flip 1st and 2nd dim
-            .ub.c = {offset[1]+readsize[1]-1, 
+
+    DBG_PRINTF("-- %s, rank %d: get data: varname=%s version=%d, lb=(%d,%d,%d) ub=(%d,%d,%d)}\n",
+        __func__, ds->mpi_rank, varname, ds->access_version, offset[1], offset[0], offset[2],
+        offset[1]+readsize[1]-1, offset[0]+readsize[0]-1, offset[2]+readsize[2]-1);
+
+    err =  dart_get (varname, ds->access_version, elemsize, 
+                     offset[1], offset[0], offset[2],
+                     offset[1]+readsize[1]-1,
                      offset[0]+readsize[0]-1,
-                     offset[2]+readsize[2]-1,}
-        }
-    };
-    strncpy(odsc.name, varname, sizeof(odsc.name));
-
-    od = obj_data_alloc_no_data(&odsc, data);
-    if (!od) {
-        error(err_no_memory, "DART obj_data_alloc_no_data() failed");  
+                     offset[2]+readsize[2]-1,
+                     data
+                    );
+    /*if (err == -ENOMEM) {
+        error(err_no_memory, "Not enough memory for DART to perform dart_get()");  
         return -err_no_memory;
-    }
-
-    DBG_PRINTF("-- %s, rank %d: get data: varname=%s odsc={.version=%d, lb=(%d,%d,%d) ub=(%d,%d,%d)}\n",
-        __func__, ds->mpi_rank, varname, odsc.version, odsc.bb.lb.c[0], odsc.bb.lb.c[1], odsc.bb.lb.c[2],
-        odsc.bb.ub.c[0], odsc.bb.ub.c[1], odsc.bb.ub.c[2]);
-    err = dcg_obj_get(od);
-    free(od);
-
-    if (err) {
-        error(err_corrupted_variable, "DART dcg_obj_get() failed to read variable %s.", varname);  
+    } 
+    else*/ if (err) {
+        error(err_corrupted_variable, "DART failed to read variable %s.", varname);  
         return -err_corrupted_variable;
     }
 
@@ -391,25 +393,28 @@ int64_t adios_read_dart_read_var (ADIOS_GROUP * gp, const char * varname,
     }
 
     /* Get type information for the variable from DataSpaces:
-       type variable name = T<varname>
+       type variable name = TYPE@<filename>/<varname>
     */
-    snprintf(dart_name, MAXDARTNAMELEN, "TYPE@%s",varname);
+    snprintf(dart_name, MAXDARTNAMELEN, "TYPE@%s/%s", ds->fname, varname);
     err = adios_read_dart_get(dart_name, adios_integer, ds, Toffset, Treadsize, &vartype);
     if (err)
         return err;
+    DBG_PRINTF("-- %s, rank %d: get type: varname=%s type=%d (%s)}\n",
+        __func__, ds->mpi_rank, dart_name, vartype, common_read_type_to_string(vartype)) ;
 
     elemsize = common_read_type_size(vartype, NULL);
     DBG_PRINTF("-- %s, rank %d: get data: varname=%s type=%d (%s) elemsize=%d}\n",
-        __func__, ds->mpi_rank, varname, vartype, common_read_type_to_string(vartype), elemsize);
+        __func__, ds->mpi_rank, dart_name, vartype, common_read_type_to_string(vartype), elemsize);
 
     total_size *= elemsize; 
 
     DBG_PRINTF("-- %s, rank %d: get data: varname=%s start=(%lld,%lld,%lld) count=(%lld,%lld,%lld)}\n",
-        __func__, ds->mpi_rank, varname, start[0], start[1], start[2], count[0], count[1], count[2]);
+        __func__, ds->mpi_rank, dart_name, start[0], start[1], start[2], count[0], count[1], count[2]);
     DBG_PRINTF("-- %s, rank %d: get data: varname=%s offset=(%d,%d,%d) readsize=(%d,%d,%d)}\n",
-        __func__, ds->mpi_rank, varname, offset[0], offset[1], offset[2], readsize[0], readsize[1], readsize[2]);
+        __func__, ds->mpi_rank, dart_name, offset[0], offset[1], offset[2], readsize[0], readsize[1], readsize[2]);
 
-    err = adios_read_dart_get(varname, vartype, ds, offset, readsize, data);
+    snprintf(dart_name, MAXDARTNAMELEN, "%s/%s", ds->fname, varname);
+    err = adios_read_dart_get(dart_name, vartype, ds, offset, readsize, data);
     if (err)
         return err;
 
