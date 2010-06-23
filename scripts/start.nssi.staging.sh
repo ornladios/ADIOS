aa3da834741e832ab0044cb0bc69874bc1ac91f6
thkorde <thkorde>
2010-06-23 16:13:39 +0000

    add loop to wait for creation of contact file.  do not rely on timers.

--- scripts/start.nssi.staging.sh
+++ scripts/start.nssi.staging.sh
@@ -6,3 +6,10 @@ SERVICE_COUNT=$1 ; shift
 SERVICE_CONFIG_FILE=$1 ; shift
 
 aprun -n $SERVICE_COUNT -N 1 $ADIOS_SRC_PATH/src/nssi-staging-server $SERVICE_CONFIG_FILE
+
+while [ ! -s $SERVICE_CONFIG_FILE ]
+  do
+  printf "%10s \r" waiting
+  sleep 1
+done
+echo "found it   "
