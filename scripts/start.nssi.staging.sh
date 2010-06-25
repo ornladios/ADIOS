932a84f2ab1d20a807ce32ff196327343ae275cd
thkorde <thkorde>
2010-06-25 18:07:31 +0000

    remove old contact info file before starting servers. don't print message in wait loop.

--- scripts/start.nssi.staging.sh
+++ scripts/start.nssi.staging.sh
@@ -5,11 +5,14 @@ export ADIOS_SRC_PATH=$HOME/projects/adios/src/adios
 SERVICE_COUNT=$1 ; shift
 SERVICE_CONFIG_FILE=$1 ; shift
 
+rm $ADIOS_NSSI_CONTACT_INFO
+
 aprun -n $SERVICE_COUNT -N 1 $ADIOS_SRC_PATH/src/nssi-staging-server $SERVICE_CONFIG_FILE
 
-while [ ! -s $SERVICE_CONFIG_FILE ]
+while [ ! -s $ADIOS_NSSI_CONTACT_INFO ]
   do
-  printf "%10s \r" waiting
+#  printf "%10s \r" waiting
   sleep 1
 done
-echo "found it   "
+echo "found $ADIOS_NSSI_CONTACT_INFO"
+cat $$ADIOS_NSSI_CONTACT_INFO
