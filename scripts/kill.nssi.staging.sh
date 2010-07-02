b6d60d7f68ee331e4153780cf457598b9e5f6ed1
thkorde <thkorde>
2010-07-02 18:52:27 +0000

    update to use env vars from the ADIOS module to find executables

--- scripts/kill.nssi.staging.sh
+++ scripts/kill.nssi.staging.sh
@@ -8,7 +8,7 @@ SVC_NID=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\1/'`
 SVC_PID=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\2/'`
 SVC_ADDR=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\3/'`
 SVC_PORT=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\4/'`
-  /ccs/home/thkorde/projects/nessie/install/x86_64/bin/nssi-kill --server-nid="$SVC_NID" --server-pid="$SVC_PID"
+  $ADIOS_DIR/bin/nssi-kill --server-nid="$SVC_NID" --server-pid="$SVC_PID"
 
 exit
 done
--- scripts/start.nssi.staging.sh
+++ scripts/start.nssi.staging.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-export ADIOS_SRC_PATH=$HOME/projects/adios/src/adios
-
 SERVICE_COUNT=$1 ; shift
 SERVICE_CONFIG_FILE=$1 ; shift
 
 rm $ADIOS_NSSI_CONTACT_INFO
 
-aprun -n $SERVICE_COUNT -N 1 $ADIOS_SRC_PATH/src/nssi-staging-server $SERVICE_CONFIG_FILE
+aprun -n $SERVICE_COUNT -N 1 $ADIOS_DIR/bin/nssi-staging-server $SERVICE_CONFIG_FILE
 
 while [ ! -s $ADIOS_NSSI_CONTACT_INFO ]
   do
-#  printf "%10s \r" waiting
   sleep 1
 done
-echo "found $ADIOS_NSSI_CONTACT_INFO"
-cat $$ADIOS_NSSI_CONTACT_INFO
