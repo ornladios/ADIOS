cd9d0730391d8fa0063bf6b9696913260780c61f
Norbert Podhorszki <pnorbert@github.com>
2010-07-08 13:47:44 +0000

    MPIRUN_NP --> NP_MPIRUN

--- tests/suite/tests/07_global_array_no_xml.sh
+++ tests/suite/tests/07_global_array_no_xml.sh
@@ -5,7 +5,7 @@
 #
 # Environment variables set by caller:
 # MPIRUN        Run command
-# MPIRUN_NP     Run commands option to set number of processes
+# NP_MPIRUN     Run commands option to set number of processes
 # MAXPROCS      Max number of processes allowed
 # HAVE_FORTRAN  yes or no
 # SRCDIR        Test source dir (.. of this script)
@@ -24,7 +24,9 @@ cp $TRUNKDIR/examples/C/global-array/adios_global_no_xml .
 cp $TRUNKDIR/examples/C/global-array/adios_read_global_no_xml .
 
 echo "Run C adios_global_no_xml"
-$MPIRUN $MPIRUN_NP $PROCS ./adios_global_no_xml
+ls -l ./adios_global_no_xml
+echo $MPIRUN $NP_MPIRUN $PROCS ./adios_global_no_xml
+$MPIRUN $NP_MPIRUN $PROCS ./adios_global_no_xml
 EX=$?
 if [ ! -f adios_global_no_xml.bp ]; then
     echo "ERROR: C version of adios_global_no_xml failed. No BP file is created. Exit code=$EX"
@@ -41,7 +43,7 @@ if [ $? != 0 ]; then
 fi
 
 echo "Run C adios_read_global_no_xml"
-$MPIRUN $MPIRUN_NP $READPROCS ./adios_read_global_no_xml | sort > c_read.txt
+$MPIRUN $NP_MPIRUN $READPROCS ./adios_read_global_no_xml | sort > c_read.txt
 EX=$?
 if [ $? != 0 ]; then
     echo "ERROR: C version of adios_read_global_no_xml exited with $EX"
@@ -68,7 +70,7 @@ cp $TRUNKDIR/examples/Fortran/global-array/adios_global adios_global_f
 cp $TRUNKDIR/examples/Fortran/global-array/adios_global.xml .
 
 echo "Run Fortran adios_global_f"
-$MPIRUN $MPIRUN_NP $PROCS ./adios_global_f
+$MPIRUN $NP_MPIRUN $PROCS ./adios_global_f
 EX=$?
 if [ ! -f adios_global.bp ]; then
     echo "ERROR: Fortran version of adios_global failed. No BP file is created. Exit code=$EX"
