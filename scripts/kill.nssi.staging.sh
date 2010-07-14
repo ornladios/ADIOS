14578d0244791049795eff9d3d3606535ab0a9b4
thkorde <thkorde>
2010-07-14 13:28:13 +0000

    update nssi-kill path for module deployment

--- scripts/kill.nssi.staging.sh
+++ scripts/kill.nssi.staging.sh
@@ -2,13 +2,11 @@
 
 SERVER_CONTACT_INFO=$1 ; shift
 
-SVC_LIST=`cat ${SERVER_CONTACT_INFO} | awk '{ ML=$0" "ML } END { print ML }'`
+SVC_LIST=`head ${SERVER_CONTACT_INFO} | awk '{ ML=$0" "ML } END { print ML }'`
 for SVC in $SVC_LIST; do
-SVC_NID=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\1/'`
-SVC_PID=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\2/'`
-SVC_ADDR=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\3/'`
-SVC_PORT=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\4/'`
-  $ADIOS_DIR/bin/nssi-kill --server-nid="$SVC_NID" --server-pid="$SVC_PID"
-
-exit
+  SVC_NID=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\1/'`
+  SVC_PID=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\2/'`
+  SVC_ADDR=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\3/'`
+  SVC_PORT=`echo $SVC | sed -e 's/\(.*\)@\(.*\)@\(.*\)@\(.*\)/\4/'`
+  $ADIOS_DIR/nssi/bin/nssi-kill --server-nid="$SVC_NID" --server-pid="$SVC_PID"
 done
