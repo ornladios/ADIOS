5ec214e07cb68a6d5348e38f37185c8172cbf323
Norbert Podhorszki <pnorbert@github.com>
2011-06-21 15:43:07 +0000

    Include config.h to know if we have HAVE_PTHREAD

--- src/staging_server/mymutex.h
+++ src/staging_server/mymutex.h
@@ -1,9 +1,12 @@
 #ifndef __MYMUTEX_H__
 #define __MYMUTEX_H__
 
+#include "config.h"
+
 #if HAVE_PTHREAD
 #   include "pthread.h"
 #else
+#   warning DEFINE FAKE PHTREAD MUTEX AND COND_VARS
 #   define pthread_mutex_t int
 #   define pthread_mutexattr_t int
     inline int pthread_mutex_init(pthread_mutex_t *mutex, pthread_mutexattr_t *attr) { return 0;}
@@ -15,6 +18,7 @@
 #   define pthread_condattr_t int
     inline int pthread_cond_init(pthread_cond_t *cond, pthread_condattr_t *attr) {return 0;}
     inline int pthread_cond_signal(pthread_cond_t *cond) {return 0;}
+    inline int pthread_cond_broadcast(pthread_cond_t *cond) {return 0;}
     inline int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex) {return 0;}
 #endif
 
