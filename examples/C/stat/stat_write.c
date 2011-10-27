bfb29aee79c69f49c0ccc23764214e3d8f569b23
Norbert Podhorszki <pnorbert@github.com>
2011-10-27 22:01:09 +0000

    Reformat source code

--- examples/C/stat/stat_write.c
+++ examples/C/stat/stat_write.c
@@ -18,50 +18,50 @@ struct double_complex
 
 int main (int argc, char ** argv) 
 {
-	char        filename [256];
-	int         rank, size, i, it;
-	int         NX = 10; 
-	double      t[NX];
-	struct double_complex c[NX];
+    char        filename [256];
+    int         rank, size, i, it;
+    int         NX = 10; 
+    double      t[NX];
+    struct double_complex c[NX];
 
-	/* ADIOS variables declarations for matching gwrite_temperature.ch */
-	int         adios_err;
-	uint64_t    adios_groupsize, adios_totalsize;
-	int64_t     adios_handle;
-	MPI_Comm    comm=MPI_COMM_WORLD;
- 
-	MPI_Init (&argc, &argv);
-	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
-	MPI_Comm_size (comm, &size);
+    /* ADIOS variables declarations for matching gwrite_temperature.ch */
+    int         adios_err;
+    uint64_t    adios_groupsize, adios_totalsize;
+    int64_t     adios_handle;
+    MPI_Comm    comm=MPI_COMM_WORLD;
 
-	adios_init ("stat.xml");
-   	strcpy (filename, "adios_stat.bp");
+    MPI_Init (&argc, &argv);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    MPI_Comm_size (comm, &size);
 
-   	for (it =0; it < 13; it++) {
-       	for (i = 0; i < NX; i++) {
-      		t[i] = it*100.0 + NX * rank + i;
+    adios_init ("stat.xml");
+    strcpy (filename, "adios_stat.bp");
+
+    for (it =0; it < 13; it++) {
+        for (i = 0; i < NX; i++) {
+            t[i] = it*100.0 + NX * rank + i;
             c[i].r = it * 10 + i + 8;
             c[i].i = it * 10 + i - 5;
-		}
-			// Introduce Inf value here	
-			t[0] = 1 / 0.0;	
+        }
+        // Introduce Inf value here    
+        t[0] = 1 / 0.0;    
 
-            if (it==0)
-		    	adios_open (&adios_handle, "temperature", filename, "w", &comm);
-            else
-		    	adios_open (&adios_handle, "temperature", filename, "a", &comm);
+        if (it==0)
+            adios_open (&adios_handle, "temperature", filename, "w", &comm);
+        else
+            adios_open (&adios_handle, "temperature", filename, "a", &comm);
 
-        	#include "gwrite_stat.ch"
-        	adios_close (adios_handle);
-			MPI_Barrier (comm);
- 	}
+#include "gwrite_stat.ch"
+        adios_close (adios_handle);
+        MPI_Barrier (comm);
+    }
 
-	printf ("[%d]: adios_stat.bp written successfully\n", rank);
-	MPI_Barrier (comm);
+    printf ("[%d]: adios_stat.bp written successfully\n", rank);
+    MPI_Barrier (comm);
     //if (rank==0) printf("Finalize adios\n");
     adios_finalize (rank);
 
-   //if (rank==0) printf("Finalize MPI\n");
+    //if (rank==0) printf("Finalize MPI\n");
     MPI_Finalize ();
-	return 0;
+    return 0;
 }
