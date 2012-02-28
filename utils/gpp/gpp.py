0a1b55e0dbad4ea781d13cb81e9c484f621f1ec7
lot <lot>
2012-02-28 20:17:40 +0000

    Major revisions to gpp.py

--- utils/gpp/gpp.py
+++ utils/gpp/gpp.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
-import os
+
 import sys
-import xml.dom.minidom
-import dircache
-import xmlparser 
-from time import sleep
+import os
 
-currentdir=os.curdir
-def callback(arg, dirname,fnames):
-    for filename in fnames: #os.listdir(dirname):
-        if(filename.endswith(".F90") or filename.endswith(".f90") or filename.endswith(".F95")
-            or filename.endswith(".f95") or filename.endswith(".c")):
-           arg.append(filename)
+import ad_config
+import type_mapper
 
 
+# checkXML attempts to call adios_lint to verify that adios will accept the xml file as valid
 def checkXML (config_file, path):
     if path == '':
         adios_lint = 'adios_lint '
@@ -30,38 +24,260 @@ def checkXML (config_file, path):
         return 'failure'
 
 
-def main(argv=None):
+def get_c_groupsize_code (group):
+    groupsize_code_string = ''
+    groupsize_code_string += 'adios_groupsize = '
+    for v in group.get_vars():
+        if v.is_scalar():
+            if v.get_type() != 'string':
+                groupsize_code_string += ('%d' % type_mapper.get_size (v.get_type() ) + ' \\\n                + ')
+            else:
+                groupsize_code_string += ('strlen(' + v.get_gwrite() + ')' + ' \\\n                + ')
+        else:
+            groupsize_code_string += ('%d * ' % type_mapper.get_size (v.get_type() ) )
 
+            for d in v.get_dimensions():
+                # need to check whether this is the timestep
+                groupsize_code_string += '(' + d + ') * '
+
+            groupsize_code_string = groupsize_code_string.rstrip ('* ')
+
+            groupsize_code_string += (' \\\n                + ')
+
+    # remove the final +\, and add the ;
+    groupsize_code_string = groupsize_code_string.rstrip('+\\\n ') + ';'
+
+    groupsize_code_string += '\nadios_group_size (adios_handle, adios_groupsize, &adios_totalsize);'
+
+    return groupsize_code_string;
+
+
+def get_fortran_groupsize_code (group):
+    groupsize_code_string = ''
+    groupsize_code_string += 'adios_groupsize = '
+    for v in group.get_vars():
+        if (v.is_scalar() ):
+            groupsize_code_string += ('%d' % type_mapper.get_size (v.get_type() ) + ' &\n                + ')
+        else:
+            groupsize_code_string += ('%d * ' % type_mapper.get_size (v.get_type() ) )
+
+            for d in v.get_dimensions():
+                # need to check whether this is the timestep
+                groupsize_code_string += '(' + d + ') * '
+
+            groupsize_code_string = groupsize_code_string.rstrip ('* ')
+
+            groupsize_code_string += (' &\n                + ')
+
+    # remove the final &+
+    groupsize_code_string = groupsize_code_string.rstrip('+\n &') 
+
+    groupsize_code_string += '\ncall adios_group_size (adios_handle, adios_groupsize, adios_totalsize, adios_err)'
+
+    return groupsize_code_string;
+
+
+def get_fortran_write_statements (group):
+
+    statements = ""
+
+    for item in group.get_ordered_contents():
+
+        if item.__class__.__name__ == 'gwrite':
+            statements += '\n' + item.get_src() 
+            continue
+
+        if item.__class__.__name__ == 'attr':
+            # Ignore the attributes for now
+            continue
+
+        # Otherwise, the item must be a variable
+        var = item
+
+        statements += '\ncall adios_write (adios_handle, "' + var.get_name() + '", ' + var.get_gwrite() + ', adios_err)'
+
+    statements += '\n'
+    return statements
+
+
+
+def get_c_write_statements (group):
+
+    statements = ""
+
+    for item in group.get_ordered_contents():
+
+        if item.__class__.__name__ == 'gwrite':
+            statements += '\n' + item.get_src() 
+            continue
+
+        if item.__class__.__name__ == 'attr':
+            # Ignore the attributes for now
+            continue
+
+        # Otherwise, the item must be a variable
+        var = item
+
+        # The tricky bit here is deciding whether we need the & before the variable name.
+        # We omit it in two cases: 1) the variable type is string, or 2) the variable is not a scalar
+        if (var.get_c_type() == 'string' or var.get_dimensions() != None):
+            var_prefix = ''
+        else:
+            var_prefix = '&'
+
+        statements += '\nadios_write (adios_handle, "' + var.get_name() + '", ' + var_prefix + var.get_gwrite() + ');'
+
+    statements += '\n'
+    return statements
+
+
+def get_fortran_read_statements (group):
+
+    statements = ""
+    statements += 'uint64_t starts[64];\n'
+    statements += 'uint64_t counts[64];\n'
+
+    for item in group.get_vars():
+
+        if var.get_dimensions() == None:
+            continue
+        i = 0
+        for dim in var.get_dimensions():
+            statements += ('starts[%d] = 0; counts[%d] = %s;\n' % (i,i,dim) )
+            i = i + 1
+
+        statements += 'adios_read_var (adios_group, "' + var.get_name() + '", starts, counts, ' + var.get_gwrite() + ');\n'
+
+    return statements
+
+
+def get_c_read_statements (group):
+
+    statements = ""
+    statements += 'uint64_t starts[64];\n'
+    statements += 'uint64_t counts[64];\n'
+
+    for var in group.get_vars():
+        if var.get_dimensions() == None:
+            continue
+        i = 0
+        for dim in var.get_dimensions():
+            statements += ('starts[%d] = 0; counts[%d] = %s;\n' % (i,i,dim) )
+            i = i + 1
+
+        # The tricky bit here is deciding whether we need the & before the variable name.
+        # We omit it in two cases: 1) the variable type is string, or 2) the variable is not a scalar
+        if (var.get_c_type() == 'string' or var.get_dimensions() != None):
+            var_prefix = ''
+        else:
+            var_prefix = '&'
+
+        statements += 'adios_read_var (adios_group, "' + var.get_name() + '", starts, counts, ' + var_prefix + var.get_gwrite() + ');\n'
+
+    return statements
+
+
+
+def generate_fortran (config):
+    #print "fortran"
+
+    # Output a gwrite_*.fh file for each group in the config
+    for group in config.get_groups():
+        # Open file - the filename is gwrite_<group>.fh
+        gwfile = open ('gwrite_' + group.get_name() + '.fh', 'w')
+
+        # Set groupsize
+        gwfile.write (get_fortran_groupsize_code (group) )
+
+        # Write vars
+        gwfile.write (get_fortran_write_statements (group) )
+
+        # Close file
+        gwfile.close()
+
+    # Output a gread_*.fh file for each group in the config
+    for group in config.get_groups():
+        # Open file - the filename is gread_<group>.fh
+        grfile = open ('gread_' + group.get_name() + '.fh', 'w')
+
+        # Set groupsize
+        grfile.write ('adios_groupsize = 0\n')
+        grfile.write ('adios_totalsize = 0\n')
+        grfile.write ('call adios_group_size (adios_handle, adios_groupsize, adios_totalsize, adios_err)\n')
+
+        # Read vars
+        
+
+
+        # Close file
+        grfile.close()
+
+
+def generate_c (config):
+    #print "c"
+
+    # Output a gwrite_*.ch file for each group in the config
+    for group in config.get_groups():
+        # Open file - the filename is gwrite_<group>.ch
+        gwfile = open ('gwrite_' + group.get_name() + '.ch', 'w')
+
+        # Set groupsize
+        gwfile.write (get_c_groupsize_code (group) )
+
+        # Write vars
+        gwfile.write (get_c_write_statements (group) )
+
+        # Close ch file
+        gwfile.close()
+
+    # Output a gread_*.ch file for each group in the config
+    for group in config.get_groups():
+        # Open file - the filename is gread_<group>.ch
+        grfile = open ('gread_' + group.get_name() + '.ch', 'w')
+
+        # Open group
+        grfile.write ('ADIOS_GROUP * adios_group = adios_gopen (adios_handle, "' + group.get_name() + '");\n')
+
+        # Read vars
+        grfile.write (get_c_read_statements (group) )
+
+        # Close group
+        grfile.write ('adios_gclose (adios_group);\n')
+
+        # Close ch file
+        grfile.close()
+
+
+def main (argv=None):
+  
+    # Must be called with one argument, the name of the xml file
     if len (sys.argv) != 2:
         print 'Usage: gpp.py <config file>\n'
         return 1
 
-    check_val = checkXML (sys.argv[1], os.path.dirname(sys.argv[0])) 
+
+    # If possible, run adios_lint to catch malformed config files
+    check_val = checkXML (sys.argv[1], os.path.dirname(sys.argv[0]))
     if check_val != 'success':
         return 1
 
-    global vardict
-    vardict = xmlparser.getVarlistFromXML(sys.argv[1])
-    for fname in vardict:
-        #line=xmlparser.sizestr[fname]
-        line = ""
-        if(xmlparser.language_group_dict[fname]==1):
-           wfile= open("gwrite_"+fname+".fh","w") 
-           rfile= open("gread_"+fname+".fh","w")
-	   rfile.write("adios_groupsize = 0\n"); 
-	   rfile.write("adios_totalsize = 0\n"); 
-        elif(xmlparser.language_group_dict[fname]==2):
-           wfile= open("gwrite_"+fname+".ch","w") 
-           rfile= open("gread_"+fname+".ch","w")
-	   rfile.write("adios_groupsize = 0;\n"); 
-	   rfile.write("adios_totalsize = 0;\n"); 
-        wfile.write(line); 
-        rfile.write(line); 
-        linerw = vardict[fname]
-        wfile.write(linerw[0])
-        rfile.write(linerw[1]) 
-        wfile.close()
-        rfile.close() 
 
-if __name__=="__main__":
-   main()
+    # The ad_config object provides all relevant info from the XML file
+    config = ad_config.adiosConfig (sys.argv[1])
+
+
+    # Now just call the generator for the specified language
+    lang = config.get_host_language().lower()
+    if lang == 'fortran':
+        generate_fortran (config)
+    elif lang == 'c' or lang == 'cpp':
+        generate_c (config)
+    else:
+        print "Fatal: Language unknown or unspecified"
+        raise SystemExit
+
+
+
+if __name__ == "__main__":
+    main()
+
