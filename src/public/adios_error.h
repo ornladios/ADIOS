380b37431899b9ece25a844e215085c80a30027b
Norbert Podhorszki <pnorbert@github.com>
2012-05-28 22:42:19 +0000

    All errors have now a negative value

--- src/public/adios_error.h
+++ src/public/adios_error.h
@@ -9,33 +9,35 @@
 #define __ADIOS_ERROR_H_
 
 enum ADIOS_ERRCODES {
-    err_no_error = 0,
-    err_no_memory,
-    err_MPI_open_error,
-    err_file_not_found,
-    err_invalid_file_pointer,
-    err_invalid_group,
-    err_invalid_group_struct,
-    err_invalid_varid,
-    err_invalid_varname,
-    err_corrupted_variable,
-    err_invalid_attrid,
-    err_invalid_attrname,
-    err_corrupted_attribute,
-    err_invalid_attribute_reference,
-    err_invalid_timestep,
-    err_no_data_at_timestep,
-    err_time_at_wrong_dimension,
-    err_invalid_read_method,
-    err_connection_failed,
-    err_out_of_bound,
-    err_operation_not_supported,
-    err_end_of_stream,    // stream: reached end of stream, returned by adios_read_open() or
-                          //         by adios_advance_step()
-    err_step_notready,    // stream: tried to advance the step, but no new step has arrived yet
-    err_too_many_files,   // DART allows for using only a fixed number of different filenames
+    err_no_error                        = 0,
+    err_no_memory                       = -1,
+    err_MPI_open_error                  = -2,
+    err_file_not_found                  = -3,
+    err_invalid_file_pointer            = -4,
+    err_invalid_group                   = -5,
+    err_invalid_group_struct            = -6,
+    err_invalid_varid                   = -7,
+    err_invalid_varname                 = -8,
+    err_corrupted_variable              = -9,
 
-    err_unspecified
+    err_invalid_attrid                  = -10,
+    err_invalid_attrname                = -11,
+    err_corrupted_attribute             = -12,
+    err_invalid_attribute_reference     = -13,
+    err_invalid_timestep                = -14,
+    err_no_data_at_timestep             = -15,
+    err_time_at_wrong_dimension         = -16,
+    err_invalid_read_method             = -17,
+    err_connection_failed               = -18,
+    err_out_of_bound                    = -19,
+
+    err_operation_not_supported         = -20,
+    err_end_of_stream                   = -21,    // stream: reached end of stream, returned by adios_read_open() or
+                          //         by adios_advance_step()
+    err_step_notready                   = -22,    // stream: tried to advance the step, but no new step has arrived yet
+    err_too_many_files                  = -23,   // DART allows for using only a fixed number of different filenames
+
+    err_unspecified                     = -24
 };
 
 void adios_error (enum ADIOS_ERRCODES errno, char *fmt, ...);
