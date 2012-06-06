9436b443703a447c1b2da13ffdceffe5b03a71ca
Norbert Podhorszki <pnorbert@github.com>
2012-06-06 17:26:26 +0000

    fix macro

--- src/core/adios_logger.h
+++ src/core/adios_logger.h
@@ -8,12 +8,15 @@
 
 extern FILE *adios_logf;
 extern int adios_verbose_level;
+extern char *adios_log_names[4];
 
 void adios_logger_init (char *logpath, int verbose_level, int rank);
 void adios_logger_finalize();
 
-#define  adios_logger(int verbose_level, ...) { \
+#define  adios_logger(verbose_level, ...) { \
     if (adios_verbose_level >= verbose_level) { \
+        if (!adios_logf) adios_logf=stderr; \
+        fprintf (adios_logf, "ADIOS %s: ",adios_log_names[verbose_level]); \
         fprintf (adios_logf, __VA_ARGS__); \
         fflush(adios_logf);\
     }\
@@ -21,8 +24,8 @@ void adios_logger_finalize();
 
 
 #define log_error(...) adios_logger(0, __VA_ARGS__)
-#define log_warn(...) adios_logger(0, __VA_ARGS__)
-#define log_info(...) adios_logger(1, __VA_ARGS__)
-#define log_debug(...) adios_logger(2, __VA_ARGS__)
+#define log_warn(...) adios_logger(1, __VA_ARGS__)
+#define log_info(...) adios_logger(2, __VA_ARGS__)
+#define log_debug(...) adios_logger(3, __VA_ARGS__)
 
 #endif
