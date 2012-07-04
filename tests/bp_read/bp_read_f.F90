638afc7097ae9f22a92a62288f587de01f50a6b6
Norbert Podhorszki <pnorbert@github.com>
2012-07-04 16:37:57 +0000

    Fortran reader with new API, also kept the old one as _v1

--- tests/bp_read/bp_read_f.F90
+++ tests/bp_read/bp_read_f.F90
@@ -6,15 +6,25 @@
 !
 
 program read_bp_f
+    use adios_read_mod
     implicit none
     include "mpif.h"
 
-    integer :: gcnt, vcnt, acnt, tstart, ntsteps, tstop, vrank, vtype, timedim, ierr
-    integer :: comm,i,j,k,l,m
-    integer*8 :: read_bytes
-    integer*8 :: fh, gh
+    integer :: gcnt, vcnt, acnt, tfirst, tlast  ! file info
+    integer :: vsteps, vrank, vtype ! variable info
+    integer :: ierr
+    integer :: comm,nproc,rank
+    integer :: i,j,k,l,m
+    integer*8 :: fh
+    integer*8 :: sel
     integer*8, dimension(10) :: dims, start, readsize
-    integer, dimension(1000) :: var, vartrue
+
+    integer :: an_int
+    real*4  :: a_real
+    real*8  :: a_double
+    complex*8  :: a_complex
+    character(len=1024) :: a_string
+
     !character, dimension(:), allocatable :: varchar
     !integer, dimension(:), allocatable :: varint
     !real*8, dimension(:), allocatable :: vardouble
@@ -22,61 +32,55 @@ program read_bp_f
     integer, dimension(100000) :: varint
     real*8, dimension(10000000) :: vardouble
     integer :: totalsize
-    character (len=100), dimension(5000) :: vnamelist
-    character (len=100), dimension(5000) :: anamelist
-    character (len=100), dimension(5000) :: gnamelist
 
-    integer :: ilom,ihip,jlom,jhip,klom,khip,nx
-    integer, dimension(3) :: vstart, readcount
-    integer,dimension(48) :: bconds  ! all boundary conditions as one array
-    character(20)    :: vname
-    integer          :: one, rank, nproc 
-    integer          :: timestep, lasttimestep
-    real*8,dimension(102,66,3)  :: b1
+    character (len=128), dimension(:), allocatable :: vnamelist
+    character (len=128), dimension(:), allocatable :: anamelist
+    character (len=128), dimension(:), allocatable :: gnamelist
+
+    character(256) :: path
 
     call MPI_Init (ierr)
     comm = MPI_COMM_WORLD
     call mpi_comm_rank(comm, rank, ierr)
     call mpi_comm_size(comm, nproc, ierr)
 
-    call adios_set_read_method (0, ierr);
+    call processArgs(path)
+
+    call adios_read_init_method (0, comm, "verbose=4", ierr);
+    call adios_read_open_file (fh, path, 0, comm, ierr)
+    if (ierr .ne. 0) then
+        call exit(1)
+    endif
+
     varchar = ' '
-    !call adios_fopen (fh, "/lustre/spider/scratch/pnorbert/TRACKP.bp", comm, ierr)
-    !call adios_fopen (fh, "TRACKP_00010.bp", comm, ierr)
-    !call adios_fopen (fh, "g_1x4_5x1.bp", comm, ierr)
-    !call adios_fopen (fh, "pixie3d.bp", comm, ierr)
-    !call adios_fopen (fh, "g_2x2_2x2_t1.bp", comm, ierr)
-    !call adios_fopen (fh, "xgcp.bp", comm, ierr)
-    !call adios_fopen (fh, "pgood.bp", comm, ierr)
-    !call adios_fopen (fh, "xgc.flowdiag.bp", comm, ierr)
-    !call adios_fopen (fh, "xgc.restart.000.03600.bp", comm, ierr)
-    !call adios_fopen (fh, "adios_global.bp", comm, gcnt, ierr)
-    call adios_fopen (fh, "testbp_c.bp", comm, gcnt, ierr)
-    !call adios_fopen (fh, "testbp.bp", comm, ierr)
-    !call adios_fopen (fh, "record.bp", comm, ierr)
-    !call adios_fopen (fh, "outxz.bp", comm, ierr)
 
-    call adios_inq_file (fh,vcnt,acnt,tstart,ntsteps,gnamelist,ierr) 
-    tstop = ntsteps+ntsteps-1
-    write (*,'("Number of timesteps : ",i0," starting from ",i0)') ntsteps, tstart
-    write (*,'("Number of groups : ",i0)') gcnt
+    call adios_inq_file (fh,vcnt,acnt,tfirst,tlast,ierr) 
+    allocate (vnamelist(vcnt))
+    allocate (anamelist(acnt))
+    call adios_inq_varnames  (fh, vnamelist, ierr)
+    call adios_inq_attrnames (fh, anamelist, ierr)
 
-    do i=1,gcnt 
-        write (*,"(i5, a, a)") i,")  ", trim(gnamelist(i))
-    enddo
-
-    call adios_gopen (fh, gh, gnamelist(1), vcnt, acnt, ierr) 
-    call adios_inq_group(gh, vnamelist, anamelist, timestep, lasttimestep, ierr)
-
-    write (*,'("Number of variables in group ",a,": ",i0)') trim(gnamelist(1)), vcnt
+    write (*,'("Number of variables  : ",i0)') vcnt
     do i=1,vcnt 
         write (*,"(i5, a, a)") i,")  ", trim(vnamelist(i))
     enddo
 
-    write (*,'("Number of attributes in group ",a,": ",i0)') trim(gnamelist(1)), acnt
+    write (*,'("Number of attributes : ",i0)') acnt
     do i=1,acnt 
         write (*,"(i5, a, a)") i,")  ", trim(anamelist(i))
     enddo
+
+    write (*,'("Timesteps            : ",i0," - ",i0)') tfirst, tlast
+    
+    ! Not required, but get the list of groupnames and print here
+    call adios_inq_ngroups (fh, gcnt, ierr)
+    allocate (gnamelist(gcnt))
+    call adios_inq_groupnames (fh, gnamelist, ierr)
+    write (*,'("Number of groups : ",i0)') gcnt
+    do i=1,gcnt 
+        write (*,"(i5, a, a)") i,")  ", trim(gnamelist(i))
+    enddo
+    deallocate (gnamelist)
 
     ! vtype 
     ! 0 byte 
@@ -95,77 +99,87 @@ program read_bp_f
     ! 54 unsigned_long
     !write (*,*) "name    ",  "  ndim    ", "    dims"
 
-    write (*,*)"-----------------------------"
-    !do i=1,vcnt 
-    !    call adios_inq_var (gh, vnamelist(i), vtype, vrank, dims, timedim, ierr)
-    !    start(1:10)=0
-    !    readsize(1:10)=1 
-    !    totalsize=1
-    !    do j=1,vrank
-    !        readsize(j) = dims(j)
-    !        totalsize=totalsize*dims(j)
-    !    enddo
-    !    write (*,'(a," ndims=",i0," type=",i0," size=",i0)') trim(vnamelist(i)),vrank,vtype,totalsize
-    !    if (vtype == 0) then
-    !        !if(allocated(varchar)) deallocate(varchar)
-    !        !allocate(varchar(totalsize))
-    !        !varchar(1)="!"
-    !        call adios_read_var (gh, vnamelist(i), start, readsize, varchar, read_bytes)
-    !    else if (vtype == 2) then
-    !        !write (*,*) "  totalsize = ", totalsize
-    !        !if (totalsize == 1) totalsize = 10
-    !        !if(allocated(varint)) then
-    !        !    write (*,*) "  dealloc varint"
-    !        !    deallocate(varint)
-    !        !endif
-    !        !write (*,*) "  alloc varint, size = ", totalsize
-    !        !allocate(varint(totalsize))
-    !        !write (*,*) "  totalsize = ", totalsize
-    !        !varint(:)=5
-    !        !print *, varint(1:10)
-    !        call adios_read_var (gh, vnamelist(i), start, readsize, varint, read_bytes)
-    !        print *, varint(1)
-    !    else if (vtype == 6) then
-    !        !if(allocated(vardouble)) deallocate(vardouble)
-    !        !allocate(vardouble(totalsize))
-    !        call adios_read_var (gh, vnamelist(i), start, readsize, vardouble, read_bytes)
-    !    else
-    !        write (*,'(a16,": Only integer or double type is handled here")') trim(vnamelist(i))
-    !    endif
-    !    if (vrank == 0) then
-    !        if (vtype == 0) then
-    !            write(*,'("    = ",a)') varchar(1:1)
-    !        else if (vtype == 2) then
-    !            write(*,'("    = ",i0)') varint(1)
-    !        else if (vtype == 6) then 
-    !            write(*,'("    = ",d20.10)') vardouble(1)
-    !        endif
-    !    else
-    !        !write (*,*)"-----------------------------"
-    !        !write (*,'(a30,t32," dimensions = [",i0,$)') vnamelist(i), dims(1)
-    !        write (*,'("   dimensions = [",i0,$)') dims(1)
-    !        do j=2,vrank
-    !            write (*,'(",",i0,$)') dims(j)
-    !        enddo
-    !        if (vtype == 0) then
-    !            write (*,'("] = ",a)') varchar
-    !        else
-    !            write (*,'("]")') 
-    !        endif
-    !    endif
-    !enddo
-
-    start(1) = 0
-    start(2) = rank
-    readsize(1) = 10
-    readsize(2) = 1
-    call adios_read_var (gh, "temperature", start, readsize, vardouble, read_bytes)
-    call adios_gclose(gh, ierr)
-    call adios_fclose(fh, ierr)
-
-    do i=1,4
-      write (*,*) vardouble(i) 
+    write (*,*)"-------- VARIABLE INFO ---------"
+    do i=1,vcnt 
+        call adios_inq_var (fh, vnamelist(i), vtype, vsteps, vrank, dims, ierr)
+        if (vrank == 0) then
+            !! scalar variable
+            sel = 0
+            write (*,'(a,"    scalar: type=",i0," steps=",i0,$)') trim(vnamelist(i)), vtype, vrank
+            if (vtype == 2) then
+                !call adios_get_scalar (fh, vnamelist(i), an_int, ierr)
+                call adios_schedule_read (fh, sel, vnamelist(i), 0, 1,  an_int, ierr)
+                call adios_perform_reads (fh, ierr)
+                write(*,'("    = ",i0)') an_int
+            else if (vtype == 5) then 
+                call adios_get_scalar (fh, vnamelist(i), a_real, ierr)
+                write(*,'("    = ",d20.10)') a_real
+            else if (vtype == 6) then 
+                call adios_get_scalar (fh, vnamelist(i), a_double, ierr)
+                write(*,'("    = ",d20.10)') a_double
+            else if (vtype == 9) then
+                call adios_get_scalar (fh, vnamelist(i), a_string, ierr)
+                write(*,'("    = ",a)') trim(a_string)
+            else if (vtype == 10) then 
+                call adios_get_scalar (fh, vnamelist(i), a_complex, ierr)
+                write(*,'("    = (",d20.10,",",d20.10,")")') a_complex
+            else
+                write (*,*) " This type is not handled here"
+            endif
+        else
+            !! array variable
+            start(1:10)=0
+            readsize(1:10)=1 
+            totalsize=1
+            write (*,'(a,"    type=",i0," ndims=",i0,"{",$)') trim(vnamelist(i)),vrank,vtype
+            do j=1,vrank
+                readsize(j) = dims(j)
+                totalsize=totalsize*dims(j)
+                write(*,'(" ",i0,$)') dims(j)
+            enddo
+            write(*,'("}")') dims(j)
+        endif
     enddo
+
+    call adios_read_close(fh, ierr)
+
+    deallocate (vnamelist)
+    deallocate (anamelist)
 
     call MPI_Finalize (ierr)
 end program
+
+!!***************************
+subroutine usage()
+    write (*,*) "Usage: bp_read_f path]"
+    write (*,*) "    path: name of bp file"
+end subroutine usage
+
+!!***************************
+subroutine processArgs(path)
+
+    implicit none
+    character(len=256), intent(out) :: path
+
+#ifndef __GFORTRAN__
+#ifndef __GNUC__
+    interface
+         integer function iargc()
+         end function iargc
+    end interface
+#endif
+#endif
+
+    integer :: numargs
+
+    !! process arguments
+    numargs = iargc()
+    !print *,"Number of arguments:",numargs
+    if ( numargs < 1 ) then
+        call usage()
+        call exit(1)
+    endif
+    call getarg(1, path)
+
+end subroutine processArgs
+
