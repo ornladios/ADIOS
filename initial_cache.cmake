84387f75a792bd2c2c944a0daa3628e15e3bf191
jya <jya>
2012-11-13 19:31:59 +0000

    fix HDF5 mpi problem

--- initial_cache.cmake
+++ initial_cache.cmake
@@ -1,7 +1,7 @@
 set(BUILD_WRITE ON CACHE BOOL "")
 set(BUILD_FORTRAN OFF CACHE BOOL "")
 
-set(MXML_DIR "" CACHE FILEPATH "path to mxml dir")
+set(MXML_DIR "/sw/analysis-x64/mxml/2.6/centos5.5_pgi10.9" CACHE FILEPATH "path to mxml dir")
 
 set(PHDF5 ON CACHE BOOL "")
 # phdf5: /sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4
@@ -9,8 +9,8 @@ set(PHDF5_DIR "/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4" CACHE FILEPATH "
 set(PHDF5_FLAGS "-I/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4/include" CACHE FILEPATH "flags to use parallel hdf5")
 set(PHDF5_LIBS "-L/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4/lib -lhdf5_hl -lhdf5 -L/sw/sith/szip/2.1/centos5.5_pgi10.9/lib -lsz -lz -lm" CACHE STRING "parallel hdf5")
 
-set(HDF5 OFF CACHE BOOL "")
-set(HDF5_DIR "/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9" FILEPATH "path to hdf5 dir")
+set(HDF5 ON CACHE BOOL "")
+set(HDF5_DIR "/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9" CACHE FILEPATH "path to hdf5 dir")
 set(HDF5_FLAGS "-I/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9/include" CACHE FILEPATH "flags to use suquential hdf5")
 set(HDF5_LIBS "-L/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9/lib -lhdf5_hl -lhdf5 -L/sw/sith/szip/2.1/centos5.5_pgi10.9/lib -lsz -lz -lm" CACHE FILEPATH "sequential hdf5")
 
