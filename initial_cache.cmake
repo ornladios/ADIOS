3631db3f41a4385f412cfb383107cbf4d25a545f
Jingqing Mu <jya@sith-login1.ccs.ornl.gov>
2013-03-18 15:48:05 -0400

    change PHDF5 and HDF5 configuration from environmental variables

--- initial_cache.cmake
+++ initial_cache.cmake
@@ -1,7 +1,8 @@
 set(BUILD_WRITE ON CACHE BOOL "")
 
-set(BUILD_FORTRAN OFF CACHE BOOL "")
+set(BUILD_FORTRAN ON CACHE BOOL "")
 
+set(MACRODEF OFF CACHE BOOL "")
 #set(MXML_DIR "/sw/analysis-x64/mxml/2.6/centos5.5_pgi10.9" CACHE FILEPATH "path to mxml dir")
 
 set(DATATAP OFF CACHE BOOL "")
@@ -16,9 +17,14 @@ set(NC4PAR OFF CACHE BOOL "")
 
 set(PHDF5 ON CACHE BOOL "")
 # phdf5: /sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4
-set(PHDF5_DIR "/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4" CACHE FILEPATH "path to parallel hdf5 dir")
-set(PHDF5_FLAGS "-I/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4/include" CACHE FILEPATH "flags to use parallel hdf5")
-set(PHDF5_LIBS "-L/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4/lib -lhdf5_hl -lhdf5 -L/sw/sith/szip/2.1/centos5.5_pgi10.9/lib -lsz -lz -lm" CACHE STRING "parallel hdf5")
+if(PHDF5)
+#  set(PHDF5_DIR "/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4" CACHE FILEPATH "path to parallel hdf5 dir")
+#  set(PHDF5_FLAGS "-I/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4/include" CACHE FILEPATH "flags to use parallel hdf5")
+#  set(PHDF5_LIBS "-L/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4/lib -lhdf5_hl -lhdf5 -L/sw/sith/szip/2.1/centos5.5_pgi10.9/lib -lsz -lz -lm" CACHE STRING "parallel hdf5")
+  set(PHDF5_DIR $ENV{PAR_HDF5_DIR} CACHE FILEPATH "path to parallel hdf5 dir")
+  set(PHDF5_FLAGS "-I${PHDF5_DIR}/include" CACHE FILEPATH "flags to use parallel hdf5")
+  set(PHDF5_LIBS $ENV{PAR_HDF5_CLIB} CACHE STRING "parallel hdf5")
+endif()
 
 set(PORTALS OFF CACHE BOOL "")
 
@@ -31,9 +37,15 @@ set(CRAY_UGNI OFF CACHE BOOL "")
 set(NETCDF OFF CACHE BOOL "")
 
 set(HDF5 ON CACHE BOOL "")
-set(HDF5_DIR "/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9" CACHE FILEPATH "path to hdf5 dir")
-set(HDF5_FLAGS "-I/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9/include" CACHE FILEPATH "flags to use suquential hdf5")
-set(HDF5_LIBS "-L/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9/lib -lhdf5_hl -lhdf5 -L/sw/sith/szip/2.1/centos5.5_pgi10.9/lib -lsz -lz -lm" CACHE FILEPATH "sequential hdf5")
+
+if(HDF5)
+#  set(HDF5_DIR "/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9" CACHE FILEPATH "path to hdf5 dir")
+#  set(HDF5_FLAGS "-I/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9/include" CACHE FILEPATH "flags to use suquential hdf5")
+#  set(HDF5_LIBS "-L/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9/lib -lhdf5_hl -lhdf5 -L/sw/sith/szip/2.1/centos5.5_pgi10.9/lib -lsz -lz -lm" CACHE FILEPATH "sequential hdf5")
+  set(HDF5_DIR $ENV{SEQ_HDF5_DIR} CACHE FILEPATH "path to hdf5 dir")
+  set(HDF5_FLAGS "-I${HDF5_DIR}/include" CACHE FILEPATH "flags to use suquential hdf5")
+  set(HDF5_LIBS $ENV{SEQ_HDF5_CLIB} CACHE FILEPATH "sequential hdf5")
+endif()
 
 set(DMALLOC OFF CACHE BOOL "")
 
