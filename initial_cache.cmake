4bf5888bd2009ba5742ea9a0e2108f35d9ecec19
Jingqing Mu <jya@sith-login1.ccs.ornl.gov>
2013-04-09 14:18:59 -0400

    add infiniband and dataspaces use cmake_init for build_write and build_fortran decision

--- initial_cache.cmake
+++ initial_cache.cmake
@@ -1,17 +1,30 @@
-set(BUILD_WRITE ON CACHE BOOL "")
+if(DEFINED ENV{BUILD_WRITE})
+  set(BUILD_WRITE $ENV{BUILD_WRITE})
+  if(BUILD_WRITE)
+    set(BUILD_WRITE ON CACHE BOOL "")
+  else()
+    set(BUILD_WRITE OFF CACHE BOOL "")
+  endif()
+else()
+  set(BUILD_WRITE OFF CACHE BOOL "")
+endif()
 
-set(BUILD_FORTRAN ON CACHE BOOL "")
+if(DEFINED ENV{BUILD_FORTRAN})
+  set(BUILD_FORTRAN $ENV{BUILD_FORTRAN})
+  if(BUILD_FORTRAN)
+    set(BUILD_FORTRAN ON CACHE BOOL "")
+  else()
+    set(BUILD_FORTRAN OFF CACHE BOOL "")
+  endif()
+else()
+  set(BUILD_FORTRAN OFF CACHE BOOL "")
+endif()
 
-#set(MACRODEF OFF CACHE BOOL "")
+set(MACRODEFFLAG $ENV{MACRODEFFLAG})
 
 set(MXML_DIR $ENV{MXML_DIR}  CACHE FILEPATH "path to mxml dir")
-#set(MXML_INC $ENV{MXML_INC} CACHE FILEPATH "path to mxml include")
-
-#set(MXML_LIB $ENV{MXML_LIB} CACHE FILEPATH "path to mxml library")
 
 set(DATATAP OFF CACHE BOOL "")
-
-set(DATASPACES OFF CACHE BOOL "")
 
 set(DIMES OFF CACHE BOOL "")
 
@@ -19,12 +32,13 @@ set(NSSI OFF CACHE BOOL "")
 
 set(NC4PAR OFF CACHE BOOL "")
 
+if(NC4PAR)
+  set(NC4PAR_DIR "$ENV{PAR_NC_DIR}" CACHE FILEPATH "path to parallel NETCDF dir")
+endif()
+
 set(PHDF5 ON CACHE BOOL "")
-# phdf5: /sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4
+
 if(PHDF5)
-#  set(PHDF5_DIR "/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4" CACHE FILEPATH "path to parallel hdf5 dir")
-#  set(PHDF5_FLAGS "-I/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4/include" CACHE FILEPATH "flags to use parallel hdf5")
-#  set(PHDF5_LIBS "-L/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9_ompi1.4.4/lib -lhdf5_hl -lhdf5 -L/sw/sith/szip/2.1/centos5.5_pgi10.9/lib -lsz -lz -lm" CACHE STRING "parallel hdf5")
   set(PHDF5_DIR $ENV{PAR_HDF5_DIR} CACHE FILEPATH "path to parallel hdf5 dir")
   set(PHDF5_FLAGS "-I${PHDF5_DIR}/include" CACHE FILEPATH "flags to use parallel hdf5")
   set(PHDF5_LIBS $ENV{PAR_HDF5_CLIB} CACHE STRING "parallel hdf5")
@@ -32,7 +46,27 @@ endif()
 
 set(PORTALS OFF CACHE BOOL "")
 
-set(INFINIBAND OFF CACHE BOOL "")
+if(DEFINED ENV{DATASPACES_INCDIR})
+  set(DATASPACES_INCDIR $ENV{DATASPACES_INCDIR} CACHE INTERNAL "Internal variable")
+  if(DATASPACES_INCDIR)
+    if(DEFINED $ENV{DATASPACES_LIBDIR})
+      set(DATASPACES_LIBDIR $ENV{DATASPACES_LIBDIR} CACHE INTERNAL "Internal variable")
+        if(DATASPACES_LIBDIR)
+          set(DATASPACES ON CACHE BOOL "")
+        endif()
+    endif()
+  endif()
+elseif(DEFINED ENV{DATASPACES_DIR})
+  set(DATASPACES ON CACHE BOOL "")
+  set(DATASPACES_DIR $ENV{DATASPACES_DIR} CACHE FILEPATH "path to dataspaces dir")
+#  set(DATASPACES_LIBS "-L$ENV{DATASPACES_DIR}/libs -ldspaces -ldscommon -ldar" CACHE STRING "dataspace libs")
+elseif(DEFINED ENV{DATASPACES})
+  set(DATASPACES ON CACHE BOOL "")
+  set(DATASPACES_DIR $ENV{DATASPACES} CACHE FILEPATH "path to dataspaces dir")
+else()
+  set(DATASPACES OFF CACHE BOOL "")
+endif()
+
 
 set(CRAY_PMI OFF CACHE BOOL "")
 
@@ -40,12 +74,13 @@ set(CRAY_UGNI OFF CACHE BOOL "")
 
 set(NETCDF OFF CACHE BOOL "")
 
+if(NETCDF)
+  set(NETCDF_DIR "$ENV{SEQ_NC_DIR}" CACHE FILEPATH "path to suquential NETCDF dir")
+endif()
+
 set(HDF5 ON CACHE BOOL "")
 
 if(HDF5)
-#  set(HDF5_DIR "/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9" CACHE FILEPATH "path to hdf5 dir")
-#  set(HDF5_FLAGS "-I/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9/include" CACHE FILEPATH "flags to use suquential hdf5")
-#  set(HDF5_LIBS "-L/sw/sith/hdf5/1.8.5/centos5.5_pgi10.9/lib -lhdf5_hl -lhdf5 -L/sw/sith/szip/2.1/centos5.5_pgi10.9/lib -lsz -lz -lm" CACHE FILEPATH "sequential hdf5")
   set(HDF5_DIR "$ENV{SEQ_HDF5_DIR}" CACHE FILEPATH "path to hdf5 dir")
   set(HDF5_FLAGS "-I${HDF5_DIR}/include" CACHE FILEPATH "flags to use suquential hdf5")
   set(HDF5_LIBS "$ENV{SEQ_HDF5_CLIB}" CACHE FILEPATH "sequential hdf5")
