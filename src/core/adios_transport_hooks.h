c9f0d8240e88a1eaca190741ccb0bd0ff9828cf4
Yuan Tian <tianyua@sith-login1.ccs.ornl.gov>
2013-04-18 15:15:25 -0400

    - checking in current code for CHUNK method - contains a lot of debug prints

--- src/core/adios_transport_hooks.h
+++ src/core/adios_transport_hooks.h
@@ -12,7 +12,6 @@
 #include <stdint.h>
 #include <string.h>
 #include "core/util.h" /* PairStruct* */
-#include "public/adios_mpi.h"
 
 #define FORWARD_DECLARE_EMPTY(a) \
 void adios_##a##_init (const PairStruct * parameters \
@@ -127,7 +126,10 @@ enum ADIOS_IO_METHOD {ADIOS_METHOD_UNKNOWN     = -2
               ,ADIOS_METHOD_NSSI_STAGING = 18
               ,ADIOS_METHOD_NSSI_FILTER  = 19
               ,ADIOS_METHOD_DIMES        = 20
-              ,ADIOS_METHOD_COUNT        = 21
+              //Tian's method
+              ,ADIOS_METHOD_CHUNK       = 21
+              ,ADIOS_METHOD_COUNT        = 22 //increased from 21 to 22 for Tian's method
+              //,ADIOS_METHOD_COUNT        = 21
 };
 
 // forward declare the functions (or dummies for internals use)
@@ -139,6 +141,8 @@ enum ADIOS_IO_METHOD {ADIOS_METHOD_UNKNOWN     = -2
      FORWARD_DECLARE(mpi_stagger)
      FORWARD_DECLARE(mpi_aggregate)
      FORWARD_DECLARE(mpi_amr)
+    //Tian's method
+     FORWARD_DECLARE(chunk)
      FORWARD_DECLARE(mpi_amr1)
      FORWARD_DECLARE(phdf5)
      FORWARD_DECLARE(nc4)
