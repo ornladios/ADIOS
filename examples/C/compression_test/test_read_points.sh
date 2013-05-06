a006c95c5c1c76999ee412d2bddf56ddde071d51
zgong <zgong@ncsu.edu>
2013-05-06 09:06:08 -0400

    add new compression test code

---
+++ examples/C/compression_test/test_read_points.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+all_bp_files=""
+
+for bp_file in `ls output/ | grep ".bp$"`
+do
+	#echo $bp_file
+	all_bp_files=$all_bp_files" output/"$bp_file
+done
+
+echo $all_bp_files
+
+./adios_read_points $all_bp_files
