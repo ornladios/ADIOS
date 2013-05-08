38ae36a67e0eacee5541c0d8f0660dce48578d8a
Jeremy Logan <lot@ornl.gov>
2013-05-08 13:28:20 -0400

    Created skel library to hold common skel functions that do not need to be generated.

---
+++ utils/skel/src/skel_xml_output.c
@@ -0,0 +1,10 @@
+#include "skel_xml_output.h"
+#include <stdio.h>
+
+int FC_FUNC_(skel_write_coarse_xml_data, SKEL_WRITE_COARSE_XML_DATA) ()
+{
+
+    fprintf (stdout, "In skel_write_coarse_xml_data\n");
+
+    return 0;
+}
---
+++ utils/skel/src/skel_xml_output.h
@@ -0,0 +1,8 @@
+#ifndef SKEL_XML_OUTPUT_H
+#define SKEL_XML_OUTPUT_H
+
+#include "config.h"
+
+int FC_FUNC_(skel_write_coarse_xml_data, SKEL_WRITE_COARSE_XML_DATA) ();
+
+#endif
