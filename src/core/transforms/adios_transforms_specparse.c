949a72a2431104d73ee049d27720285a315fb32c
David Boyuka II <daboyuka@ncsu.edu>
2013-05-20 13:26:50 -0400

    Fixed #includes some more

--- src/core/transforms/adios_transforms_specparse.c
+++ src/core/transforms/adios_transforms_specparse.c
@@ -7,7 +7,6 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-#include "adios_types.h"
 #include "core/transforms/adios_transforms_specparse.h"
 
 inline static char * strsplit(char *input, char split) {
