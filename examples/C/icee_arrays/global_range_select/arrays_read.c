2c042fa1ce36a9bf866573ba65243abb987303ee
Jong Choi <choij@ornl.gov>
2014-02-25 16:24:14 -0500

    Add an example for ICEE method

---
+++ examples/C/icee_arrays/global_range_select/arrays_read.c
@@ -0,0 +1,144 @@
+/* 
+ * ADIOS is freely available under the terms of the BSD license described
+ * in the COPYING file in the top level directory of this source distribution.
+ *
+ * Copyright (c) 2008 - 2009.  UT-BATTELLE, LLC. All rights reserved.
+ */
+
+/*************************************************************/
+/*          Example of reading arrays in ADIOS               */
+/*    which were written from the same number of processors  */
+/*                                                           */
+/*        Similar example is manual/2_adios_read.c           */
+/*************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include "mpi.h"
+#include "adios.h"
+#include "public/adios_read.h"
+
+int main (int argc, char ** argv) 
+{
+    char        filename [256];
+    int         rank, size, i, j;
+    int         NX, NY; 
+    double      *t;
+    MPI_Comm    comm = MPI_COMM_WORLD;
+
+    int         adios_err;
+    int64_t     adios_handle, adios_buf_size;
+    //enum ADIOS_READ_METHOD method = ADIOS_READ_METHOD_BP;
+    enum ADIOS_READ_METHOD method = ADIOS_READ_METHOD_ICEE;
+
+    char        initstring [256];
+    int         cm_port = 59999;
+
+    MPI_Init (&argc, &argv);
+    MPI_Comm_rank (comm, &rank);
+
+    sprintf(initstring, "cm_port=%d", cm_port + rank);
+    
+    adios_read_init_method(method, comm, initstring);
+
+    ADIOS_SELECTION *global_range_select;
+    
+    ADIOS_SELECTION scalar_block_select;
+    scalar_block_select.type = ADIOS_SELECTION_WRITEBLOCK;
+    scalar_block_select.u.block.index = rank;
+    
+    /* schedule_read of a scalar. */    
+    int test_scalar = -1;
+
+    ADIOS_FILE* afile = adios_read_open("arrays", 
+                                        method, 
+                                        comm,
+                                        ADIOS_LOCKMODE_NONE, 100.0);
+    
+    for(i=0; i<afile->nvars; i++){
+     	printf("var: %s\n", afile->var_namelist[i]);
+    }
+    
+    int ii = 0;
+    int ierr = 0;
+    while(ierr >= 0) {
+        /* get a bounding box - rank 0 for now*/
+        ADIOS_VARINFO *nx_info = adios_inq_var( afile, "scalar/dim/NX");
+        ADIOS_VARINFO *ny_info = adios_inq_var( afile, "scalar/dim/NY");
+        ADIOS_VARINFO *size_info = adios_inq_var( afile, "size");
+        ADIOS_VARINFO *arry = adios_inq_var( afile, "var_2d_array");
+    
+        int nx_val = *((int*)nx_info->value);
+        int ny_val = *((int*)ny_info->value);
+        int size_val = *((int*)size_info->value);
+    
+        printf("nx: %d, ny: %d, size: %d\n", nx_val, ny_val, size_val);
+	
+        uint64_t xcount = nx_val;
+        uint64_t ycount = ny_val;
+        //uint64_t xcount = arry->dims[0];
+        //uint64_t ycount = arry->dims[1];
+    
+        uint64_t starts[] = {0,rank * ny_val};
+        uint64_t counts[] = {xcount, ycount};
+        
+        global_range_select = adios_selection_boundingbox(2, starts, counts);
+    
+        int nelem = xcount*ycount;
+    
+        if(nx_info->value) {
+            NX = *((int *)nx_info->value);
+        }
+        if(ny_info->value){
+            NY= *((int*)ny_info->value);
+        }
+    
+        if(rank == 0){
+            int n;
+            printf("dims: [ ");
+            for(n=0; n<arry->ndim; n++){
+                printf("%d ", (int)arry->dims[n]);
+            }
+            printf("]\n");
+        }
+    
+        /* Allocate space for the arrays */
+        int arr_size = sizeof(double) * nelem;
+        t = (double *) malloc (arr_size);
+        memset(t, 0, arr_size);
+        //fprintf(stderr, "t %p\n", t);
+    
+        /* Read the arrays */        
+        adios_schedule_read (afile, 
+                             global_range_select, 
+                             "var_2d_array", 
+                             0, 1, t);
+        adios_schedule_read (afile,
+                             &scalar_block_select,
+                             "test_scalar",
+                             0, 1, &test_scalar);
+        adios_perform_reads (afile, 1);                
+
+        printf("Rank=%d: test_scalar: %d step: %d, t[0,5+x] = [", rank, test_scalar, ii);
+        for(j=0; j<nelem; j++) {
+            printf(", %6.2f", t[j]);
+        }
+        printf("]\n\n");
+    
+        adios_release_step(afile);
+        ierr = adios_advance_step(afile, 0, 30);
+
+        ii++;
+        MPI_Barrier (comm);
+        //sleep(1);
+    }
+    
+    adios_read_close(afile);
+
+    adios_read_finalize_method(method);
+
+    MPI_Finalize ();
+
+    return 0;
+}
---
+++ examples/C/icee_arrays/global_range_select/arrays_write.c
@@ -0,0 +1,84 @@
+/* 
+ * ADIOS is freely available under the terms of the BSD license described
+ * in the COPYING file in the top level directory of this source distribution.
+ *
+ * Copyright (c) 2008 - 2009.  UT-BATTELLE, LLC. All rights reserved.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "mpi.h"
+#include "adios.h"
+
+/*************************************************************/
+/*          Example of writing arrays in ADIOS               */
+/*                                                           */
+/*        Similar example is manual/2_adios_write.c          */
+/*************************************************************/
+int main (int argc, char ** argv) 
+{
+    char        filename [256];
+    int         rank, size, i, j, offset, size_y;
+    int         NX = 40; 
+    int         NY = 2;
+    double      t[NX*NY];
+    MPI_Comm    comm = MPI_COMM_WORLD;
+
+    int64_t     adios_handle;
+    uint64_t    adios_groupsize, adios_totalsize;
+
+    MPI_Init (&argc, &argv);
+    MPI_Comm_rank (comm, &rank);
+    MPI_Comm_size (comm, &size);
+    
+    strcpy (filename, "arrays");
+    adios_init ("arrays.xml", comm);
+    
+    int test_scalar = rank * 1000;
+    offset = rank*NY;
+    size_y = size*NY;
+    int ii;
+    
+    for(ii = 0; ii<2; ii++){       
+        for(j=0; j<NY*NX; j++){       
+            t[j] = (offset * NX) + j + NY*NX*ii;	    
+        }
+    
+        //prints the array.
+        if (ii == 0)
+            adios_open (&adios_handle, "temperature", filename, "w", comm);
+        else
+            adios_open (&adios_handle, "temperature", filename, "a", comm);
+
+        adios_groupsize = 4 \
+            + 4 \
+            + 4 \
+            + 4 \
+            + 4 \
+            + 4 \
+            + 4 \
+            + 8 * (NX) * (NY);
+        adios_group_size (adios_handle, adios_groupsize, &adios_totalsize);
+        adios_write (adios_handle, "scalar/dim/NX", &NX);
+        adios_write (adios_handle, "scalar/dim/NY", &NY);
+        adios_write (adios_handle, "size", &size);
+        adios_write (adios_handle, "rank", &rank);
+        adios_write (adios_handle, "offset", &offset);
+        adios_write (adios_handle, "size_y", &size_y);
+        adios_write (adios_handle, "test_scalar", &test_scalar);
+        adios_write (adios_handle, "var_2d_array", t);
+
+        adios_close (adios_handle);
+
+        fprintf(stderr, "Rank=%d commited write %d\n", rank, ii);
+        printf("rank %d: [", rank);
+        //for(i=0; i<NX*NY;i++){
+        printf("%lf, ", t[0]);
+		//}
+        printf("]\n");
+    }
+
+    adios_finalize (rank);
+    MPI_Finalize ();
+    return 0;
+}
