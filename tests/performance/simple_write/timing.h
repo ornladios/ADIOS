c0c2a0e2479145e4215554c2ca5fb711749ba89c
Norbert Podhorszki <pnorbert@ornl.gov>
2014-03-20 11:34:24 -0400

    Separate setup program, test input is number of comp/comm units now

--- tests/performance/simple_write/timing.h
+++ tests/performance/simple_write/timing.h
@@ -1,3 +1,4 @@
+#include "mpi.h"
 
 /* Code execution time separated into calculation and communication time 
  * They are filled out in main()
@@ -12,5 +13,5 @@ double *Tio_open, *Tio_group, *Tio_write, *Tio_close;
 // allocates and zeros out timing variables
 void timing_alloc (int nsteps); 
 void timing_free ();
-void timing_report (int nsteps); //write the report
+void timing_report (int nsteps, MPI_Comm comm); //write the report
 
