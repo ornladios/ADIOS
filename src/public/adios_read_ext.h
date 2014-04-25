81db7c1c284ac70e21800492dbceea6effa1cfd9
David Boyuka II <daboyuka@ncsu.edu>
2014-04-25 16:17:24 -0400

    Added new ADIOS header file, adios_read_ext.h, which will include experimental extensions to the core public API. Someday these functions may be transitioned to the main API; as it is, they are subject to changes at any time

---
+++ src/public/adios_read_ext.h
@@ -0,0 +1,14 @@
+/*
+ * adios_read_ext.h
+ *
+ *  Created on: Apr 25, 2014
+ *      Author: David A. Boyuka II
+ */
+#ifndef ADIOS_READ_EXT_H_
+#define ADIOS_READ_EXT_H_
+
+
+
+
+
+#endif /* ADIOS_READ_EXT_H_ */
