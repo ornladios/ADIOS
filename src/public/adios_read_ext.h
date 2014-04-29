0d2fc28689a2323dcd54187709c4e8dadfc1f8fd
David Boyuka II <daboyuka@ncsu.edu>
2014-04-28 20:21:57 -0400

    Refactored new, extended ADIOS Read API. Still need to modify ADIOS_VARINFO if Garynorbert says it's OK

--- src/public/adios_read_ext.h
+++ src/public/adios_read_ext.h
@@ -12,38 +12,54 @@
 #include "adios_read.h"
 #include "adios_selection.h"
 
-// Sets whether the transform layer is enabled for the given ADIOS file descriptor. The default is
-// enabled (true) for newly-opened file descriptors, in which case all Read API functions operate
-// on the user's view of the data.
-//
-// If the transform layer is disabled, the internal, 1D-byte-array nature of transformed variables,
-// as seen by the at the transport layer, will be exposed via Read API functions. adios_inq_var
-// and adios_inq_var_blockinfo will return this internal view of transformed variables, and
-// adios_schedule_read will operate on the 1D byte local array coordinate space, meaning only
-// ADIOS_WRITEBLOCK selections are useful, and raw, transformed data will be returned into the user
-// buffer by these read functions.
-//
-// Note: Read API functions operating on non-transformed variables are completely unaffected by this flag
-void adios_read_set_transforms_enabled(ADIOS_FILE *fp, int enabled);
+// An opaque type defining a particular view of the data.
+// Currently, there are only two possible values: LOGICAL_DATA_VIEW and PHYSICAL_DATA_VIEW
+typedef void* data_view_t;
+
+// LOGICAL_DATA_VIEW: the default, ADIOS presents the same view of the data as it was written to the
+//   file (e.g., if processes wrote to a 3D global array of doubles, the user API will present a 3D global
+//   array of doubles to the user when in this view mode).
+extern const data_view_t LOGICAL_DATA_VIEW;
+
+// PHYSICAL_DATA_VIEW: ADIOS will present the raw transport layer view of the data. If a variable is not
+//   transformed, its presentation is equivalent to that under LOGICAL_DATA_VIEW. If a variable is
+//   transformed, it will be presented as a 1D byte array, and reads will be answered directly from the
+//   transformed data with out any de-transformation applied.
+extern const data_view_t PHYSICAL_DATA_VIEW;
+
+// An identifier for a particular transform type (e.g., identity, zlib, etc.)
+// constant NO_TRANSFORM indicates the absence of any data transform being applied.
+typedef int adios_transform_type_t;
+
+extern const adios_transform_type_t NO_TRANSFORM;
+
+// A transform information structure describing how a particular variable has been transformed
+typedef struct {
+	adios_transform_type_t transform_type; /* The data transform applied to this variable */
+
+	struct {
+		const void *content; /* The transform plugin-specific metadata buffer for a given varblock */
+		uint64_t length;     /* The number of bytes in the above "content" field */
+	} *transform_metadatas; /* An array of transform plugin-specific metadata buffers, one for each
+	                           varblock in this file (number of varblocks == ADIOS_VARINFO.sum_nblocks).
+	                           Only needed by advanced applications requiring direct manipulation
+	                           of transformed data. */
+} ADIOS_VARTRANSFORM;
+
+// Sets the "data view" for this ADIOS file, which determines how ADIOS presents variables through
+// adios_inq_var*, and how reads are evaluated in adios_schedule_reads/adios_check_reads calls.
+// Currently, the choice is between a logical and physical view of the data, which only differ for
+// transformed variables; a logical view of a transformed variable presents the data as it was
+// originally written (this is the default), whereas a physical view presents the transformed data
+// as it actually exists on disk.
+void adios_read_set_data_view(ADIOS_FILE *fp, data_view_t vt);
+
+// Populates data transform information about a given variable into an existing ADIOS_VARINFO struct
+int adios_inq_var_transform(const ADIOS_FILE *fp, ADIOS_VARINFO *varinfo);
 
 // Creates a writeblock selection that only retrieves elements [start_elem, start_elem + num_elems)
 // within a variable. An element is a single value of whatever the varaible's datatype is (i.e.,
 // 1 element = 1 double if the variable type is double, 1 byte if the variable type is byte, etc.)
 ADIOS_SELECTION * adios_selection_writeblock_bounded(int index, uint64_t start_elem, uint64_t num_elems);
-
-
-// Returns the user's view of a given variable, regardless of whether transforms are enabled for the given ADIOS file descriptor
-ADIOS_TRANSINFO * adios_inq_transinfo(const ADIOS_FILE *fp, const ADIOS_VARINFO *vi);
-
-// Stores the user's view of a given variable's PG bounds into ti->orig_blockinfo, regardless of
-// whether transforms are enabled for the given ADIOS file descriptor
-// Returns >0 on success
-int adios_inq_trans_blockinfo(const ADIOS_FILE *fp, const ADIOS_VARINFO *vi, ADIOS_TRANSINFO * ti);
-
-// Returns the transform-specific metadata associated with all PGs of a variable into
-// ti->transform_metadatas and ti->transform_metadatas
-// Returns >0 on success
-int adios_inq_trans_metadata(const ADIOS_FILE *fp, const ADIOS_VARINFO *vi, ADIOS_TRANSINFO * ti);
-
 
 #endif /* ADIOS_READ_EXT_H_ */
