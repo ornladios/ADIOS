232d31e33020e4b98fb0bbb109f02a0d4c169d90
David Boyuka II <daboyuka@ncsu.edu>
2014-09-12 03:17:35 -0400

    fixup! Changed the way ADIOS Transforms computes the worst case group size after data transforms. This method should provide a much tighter upper bound

--- src/core/transforms/adios_transforms_write.c
+++ src/core/transforms/adios_transforms_write.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
 
 #include "adios_bp_v1.h"
 #include "common_adios.h"
@@ -70,8 +71,8 @@ uint64_t adios_transform_worst_case_transformed_group_size(uint64_t group_size, 
 
     const uint64_t max_transformed_group_size =
     		total_constant_factor +
-    		max_linear_factor * transformed_group_size +
-    		max_capped_linear_factor * MAX(transformed_group_size, max_capped_linear_cap);
+    		ceil(max_linear_factor * transformed_group_size) +
+    		ceil(max_capped_linear_factor * MAX(transformed_group_size, max_capped_linear_cap));
 
     // Return the maximum worst case for the group size
     // (which can never be less than the starting group size)
