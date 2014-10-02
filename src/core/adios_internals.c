034407a24f09ec0ff20ae57833bad5e583b7e668
Norbert Podhorszki <pnorbert@ornl.gov>
2014-10-02 11:25:51 -0400

    valgrind find: initialize attr->write_offset at definition time

--- src/core/adios_internals.c
+++ src/core/adios_internals.c
@@ -976,6 +976,7 @@ int adios_common_define_attribute (int64_t group, const char * name
     }
 
     attr->next = 0;
+    attr->write_offset = 0;
 
     adios_append_attribute (&g->attributes, attr, ++g->member_count);
 
@@ -1048,6 +1049,7 @@ int adios_common_define_attribute_byvalue (int64_t group, const char * name
     }
 
     attr->next = 0;
+    attr->write_offset = 0;
 
     adios_append_attribute (&g->attributes, attr, ++g->member_count);
 
