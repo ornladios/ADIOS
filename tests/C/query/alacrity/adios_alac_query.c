61e9ea3afada74d3cb2cd476f546ed165d36f492
David Boyuka II <daboyuka@ncsu.edu>
2014-10-07 23:25:48 -0400

    fixup! Refactoring: removed adios_query_init() and adios_query_finalize(). read init will call common_query_init but that does not call methods’ inits common_query_finalize calls all methods’ finalize Fortran library friendly automatic discovery of available method + adios_query_set_method()

--- tests/C/query/alacrity/adios_alac_query.c
+++ tests/C/query/alacrity/adios_alac_query.c
@@ -128,16 +128,13 @@ int main(int argc, char ** argv) {
         strcpy(xmlFileName,  argv[2]);
     }
 
+    enum ADIOS_QUERY_METHOD query_method = ADIOS_QUERY_METHOD_UNKNOWN;
     if (strcmp(argv[3], "ALACRITY") == 0) {
-    	// TODO: we no longer select query methods via adios_query_init, so replace this
-    	// with the proper method for selecting a query method once it has been decided
-        //adios_query_init(ADIOS_QUERY_METHOD_ALACRITY);
+    	query_method = ADIOS_QUERY_METHOD_ALACRITY;
     	abort();
     }
     else if (strcmp(argv[3], "FASTBIT") == 0) {
-    	// TODO: we no longer select query methods via adios_query_init, so replace this
-    	// with the proper method for selecting a query method once it has been decided
-    	abort();
+    	query_method = ADIOS_QUERY_METHOD_FASTBIT;
     }
     else {
         printf("Unsupported query engine, exiting...\n");
@@ -159,6 +156,7 @@ int main(int argc, char ** argv) {
     queryInfo = parseXml(xmlFileName, f);
 
     // perform query
+    adios_query_set_method(queryInfo->query, query_method);
     performQuery(queryInfo, f);
 
 
