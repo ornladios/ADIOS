b90806585a190a2ec20ca971b3d0264c42ce712e
guj <jgu@lbl.gov>
2014-10-23 14:52:11 -0700

    mac does not have clock_gettime so add a patch for that

--- src/query/query_fastbit.c
+++ src/query/query_fastbit.c
@@ -9,6 +9,23 @@
 #include <iapi.h>
 #include <math.h>
 
+#ifdef __MACH__
+#include <mach/mach_time.h>
+#define CLOCK_REALTIME 0
+#define CLOCK_MONOTONIC 0
+int clock_gettime(int clk_id, struct timespec *t){
+  mach_timebase_info_data_t timebase;
+  mach_timebase_info(&timebase);
+  uint64_t time;
+  time = mach_absolute_time();
+  double nseconds = ((double)time * (double)timebase.numer)/((double)timebase.denom);
+  double seconds = ((double)time * (double)timebase.numer)/((double)timebase.denom * 1e9);
+  t->tv_sec = seconds;
+  t->tv_nsec = nseconds;
+  return 0;
+}
+#endif
+
 typedef struct {
   double* _keys; 
   int64_t* _offsets; 
