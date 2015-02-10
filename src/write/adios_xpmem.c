9c847203aaa278d312c21260b653e137e718e7cd
Hasan <habbasi@ornl.gov>
2015-02-10 15:47:28 -0700

    added finalizing segments

--- src/write/adios_xpmem.c
+++ src/write/adios_xpmem.c
@@ -267,6 +267,24 @@ void adios_xpmem_finalize (int mype, struct adios_method_struct * method)
 {
 	adios_xpmem_data_struct * p = (adios_xpmem_data_struct *)
 		method->method_data;
+
+	//set the finalize flag on the shared segment
+	p->sp->finalized = 1;
+	
+	//now loop over the readcount
+	while(p->sp->readcount != 1)
+		adios_nanosleep(0, 100000000);
+
+	//now loop on finalized until it is set to 2
+	while(p->sp->finalized != 2)
+		adios_nanosleep(0, 100000000);
+
+	//unmake the segment
+	unmake_share(p->buffer_id, p->b);
+
+	//now we can delete shit and exit
+	free(p);
+	
 	if (adios_xpmem_initialized)
 		adios_xpmem_initialized = 0;
 }
