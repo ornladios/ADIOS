5ea15205bd27ff3097f928afa36f1a4350be242b
Hasan <habbasi@ornl.gov>
2015-02-09 17:38:49 -0700

    transforms still screwing me

--- src/read/read_xpmem.c
+++ src/read/read_xpmem.c
@@ -821,7 +821,7 @@ adios_read_xpmem_free_varinfo (ADIOS_VARINFO *adiosvar)
 
 
 ADIOS_TRANSINFO* 
-adios_read_flexpath_inq_var_transinfo(const ADIOS_FILE *gp, const ADIOS_VARINFO *vi)
+adios_read_xpmem_inq_var_transinfo(const ADIOS_FILE *gp, const ADIOS_VARINFO *vi)
 {    
     //adios_error(err_operation_not_supported, "Flexpath does not yet support transforms: var_transinfo.\n");
     ADIOS_TRANSINFO *trans = malloc(sizeof(ADIOS_TRANSINFO));
