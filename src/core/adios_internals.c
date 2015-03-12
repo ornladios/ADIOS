5930375ac04fcf44e711a8645d60e4291c9cc3f3
Norbert Podhorszki <pnorbert@ornl.gov>
2015-03-12 14:02:35 -0400

    fix: PG appending should not clear item's next, as it may be a list of PGs itself

--- src/core/adios_internals.c
+++ src/core/adios_internals.c
@@ -2228,8 +2228,12 @@ static void index_append_process_group_v1 (
         // first element
         index->pg_root = item;
     }
-    item->next = 0;
     index->pg_tail = item;
+    /* item may be head of a long list of PGs 
+       (e.g. during global index aggregation in MPI_AGGREGATE)
+       So don't do this: 
+          item->next = 0; 
+    */
 }
 
 static void index_append_var_v1 (
@@ -2484,6 +2488,8 @@ void adios_merge_index_v1 (
                   )
 {
     // this will just add it on to the end and all should work fine
+    log_warn ("MERGE PG name=%s, pid=%d time=%d, offset=%lld\n", 
+            new_pg_root->group_name, new_pg_root->process_id, new_pg_root->time_index, new_pg_root->offset_in_file);
     index_append_process_group_v1 (main_index, new_pg_root);
 
     // need to do vars attrs one at a time to merge them properly
@@ -3317,6 +3323,8 @@ void adios_build_index_v1 (struct adios_file_struct * fd,
     g_item->next = 0;
 
     // build the groups and vars index
+    log_warn ("APPEND PG name=%s, pid=%d time=%d, offset=%lld\n", 
+            g_item->group_name, g_item->process_id, g_item->time_index, g_item->offset_in_file);
     index_append_process_group_v1 (index, g_item);
 
     while (v)
