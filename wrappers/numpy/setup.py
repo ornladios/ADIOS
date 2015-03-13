a25cb33e10245f68606242e72e443f91771433f0
Jong Choi <choij@ornl.gov>
2015-03-13 15:02:50 -0400

    Update version number: 1.0.6

--- wrappers/numpy/setup.py
+++ wrappers/numpy/setup.py
@@ -67,7 +67,7 @@ class adios_test(Command):
         test_runner.run(test_suite)
 
 setup(name = 'adios',
-      version = '1.0.5',
+      version = '1.0.6',
       description = 'Python Module for Adios',
       author = 'Jong Choi',
       author_email = 'yyalli@gmail.com',
--- wrappers/numpy/setup_mpi.py
+++ wrappers/numpy/setup_mpi.py
@@ -57,7 +57,7 @@ class adios_test(Command):
         raise SystemExit(errno)
     
 setup(name = 'adios_mpi',
-      version = '1.0.5',
+      version = '1.0.6',
       description = 'Python Module for Adios MPI',
       author = 'Jong Choi',
       author_email = 'yyalli@gmail.com',
