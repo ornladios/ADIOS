9f69d7a7993a5af1981e3b2c27fb8f39d0914e64
Jong Choi <choij@ornl.gov>
2015-08-14 13:11:51 -0400

    Updated version number for PyPI

--- wrappers/numpy/setup.py
+++ wrappers/numpy/setup.py
@@ -67,7 +67,7 @@ class adios_test(Command):
         test_runner.run(test_suite)
 
 setup(name = 'adios',
-      version = '1.8.1b3',
+      version = '1.9.0',
       description = 'Python Module for Adios',
       author = 'Jong Choi',
       author_email = 'yyalli@gmail.com',
--- wrappers/numpy/setup_mpi.py
+++ wrappers/numpy/setup_mpi.py
@@ -123,7 +123,7 @@ class adios_test(Command):
         raise SystemExit(errno)
     
 setup(name = 'adios_mpi',
-      version = '1.8.1b3',
+      version = '1.9.0',
       description = 'Python Module for Adios MPI',
       author = 'Jong Choi',
       author_email = 'yyalli@gmail.com',
