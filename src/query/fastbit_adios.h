ede269cf0c8d921500b0dc0e4576d7d7c5905658
Norbert Podhorszki <pnorbert@ornl.gov>
2016-04-29 16:47:42 -0400

    Move clock_gettime() definition into .c from .h to avoid duplicate errors at link time.

--- src/query/fastbit_adios.h
+++ src/query/fastbit_adios.h
@@ -16,22 +16,12 @@ extern "C" {
 #include "query_utils.h"
 
 #ifdef __MACH__
-#include <mach/mach_time.h>
-#define CLOCK_REALTIME 0
-#define CLOCK_MONOTONIC 0
-int clock_gettime(int clk_id, struct timespec *t){
-  mach_timebase_info_data_t timebase;
-  mach_timebase_info(&timebase);
-  uint64_t time;
-  time = mach_absolute_time();
-  double nseconds = ((double)time * (double)timebase.numer)/((double)timebase.denom);
-  double seconds = ((double)time * (double)timebase.numer)/((double)timebase.denom * 1e9);
-  t->tv_sec = seconds;
-  t->tv_nsec = nseconds;
-  return 0;
-}
+#  include <mach/mach_time.h>
+#  define CLOCK_REALTIME 0
+#  define CLOCK_MONOTONIC 0
+int clock_gettime(int clk_id, struct timespec *t);
 #else
-#include <time.h>
+#  include <time.h>
 #endif
 
 #include <math.h>
