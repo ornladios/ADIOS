f0099aa1ed4cb01d4aaf9984d4b4c175b7fdddd2
Norbert Podhorszki <pnorbert@ornl.gov>
2016-11-14 12:43:56 -0500

    When buffer reallocation fails because size > max allowed size, still try to allocate up to the max allowed size

--- examples/C/global-array/adios_global_no_xml.c
+++ examples/C/global-array/adios_global_no_xml.c
@@ -20,6 +20,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 #include <string.h>
 #include "mpi.h"
@@ -34,8 +35,8 @@ int main (int argc, char ** argv)
 {
 	char        filename [256];
 	int         rank, size, i, block;
-	int         NX = 100, Global_bounds, Offsets; 
-	double      t[NX];
+	int         NX = 10000000, Global_bounds, Offsets;
+	double     *t;
 	int         sub_blocks = 3;
 	int64_t     var_ids[sub_blocks];
 	MPI_Comm    comm = MPI_COMM_WORLD;
@@ -47,81 +48,83 @@ int main (int argc, char ** argv)
 	MPI_Comm_rank (comm, &rank);
 	MPI_Comm_size (comm, &size);
 
-        Global_bounds = sub_blocks * NX * size;
+	t = (double *) malloc (NX * sizeof(double));
+	Global_bounds = sub_blocks * NX * size;
 
 	strcpy (filename, "adios_global_no_xml.bp");
 
 	adios_init_noxml (comm);
-        adios_set_max_buffer_size (10);
+	adios_set_max_buffer_size (sub_blocks*NX*sizeof(double)/1048576 + 2);
 
-        int64_t       m_adios_group;
-        int64_t       m_adios_file;
+	int64_t       m_adios_group;
+	int64_t       m_adios_file;
 
-        adios_declare_group (&m_adios_group, "restart", "iter", adios_stat_default);
-        adios_select_method (m_adios_group, "MPI", "", "");
+	adios_declare_group (&m_adios_group, "restart", "iter", adios_stat_default);
+	adios_select_method (m_adios_group, "MPI", "verbose=3", "");
 
 
-        adios_define_var (m_adios_group, "NX"
+	adios_define_var (m_adios_group, "NX"
 			,"", adios_integer
 			,0, 0, 0);
-   
+
 	adios_define_var (m_adios_group, "Global_bounds"
 			,"", adios_integer
 			,0, 0, 0);
 
-        for (i=0;i<sub_blocks;i++) {
-   
-           adios_define_var (m_adios_group, "Offsets"
-                        ,"", adios_integer
-                        ,0, 0, 0);
-   
-           var_ids[i] = adios_define_var (m_adios_group, "temperature"
-                        ,"", adios_double
-                        ,"NX", "Global_bounds", "Offsets");
-           adios_set_transform (var_ids[i], "identity");
+	for (i=0;i<sub_blocks;i++) {
 
-           /* This is here just for test and will cause errors.
-            * adios_expected_var_size() does not work here because the definition of the variable depends
-            * on the "NX" dimension variable and it's value known to adios only after adios_write("NX")
-            */
-           /*
+		adios_define_var (m_adios_group, "Offsets"
+				,"", adios_integer
+				,0, 0, 0);
+
+		var_ids[i] = adios_define_var (m_adios_group, "temperature"
+				,"", adios_double
+				,"NX", "Global_bounds", "Offsets");
+		adios_set_transform (var_ids[i], "identity");
+
+		/* This is here just for test and will cause errors.
+		 * adios_expected_var_size() does not work here because the definition of the variable depends
+		 * on the "NX" dimension variable and it's value known to adios only after adios_write("NX")
+		 */
+		/*
            uint64_t varsize = adios_expected_var_size(var_ids[i]);
            fprintf (stderr, "Temperature block %d is %" PRIu64 " bytes\n", i, varsize);
-           */
+		 */
 
-        }
-   
-        adios_open (&m_adios_file, "restart", filename, "w", comm);
+	}
 
-        adios_groupsize = sub_blocks * (4 + 4 + 4 + NX * 8);
+	adios_open (&m_adios_file, "restart", filename, "w", comm);
 
-        adios_group_size (m_adios_file, adios_groupsize, &adios_totalsize);
+	adios_groupsize = sub_blocks * (4 + 4 + 4 + NX * 8);
+
+	adios_group_size (m_adios_file, adios_groupsize, &adios_totalsize);
 	adios_write(m_adios_file, "NX", (void *) &NX);
 	adios_write(m_adios_file, "Global_bounds", (void *) &Global_bounds);
-/* now we will write the data for each sub block */
-        for (block=0;block<sub_blocks;block++) {
+	/* now we will write the data for each sub block */
+	for (block=0;block<sub_blocks;block++) {
 
-           Offsets = rank * sub_blocks * NX + block*NX;
-           adios_write(m_adios_file, "Offsets", (void *) &Offsets);
+		Offsets = rank * sub_blocks * NX + block*NX;
+		adios_write(m_adios_file, "Offsets", (void *) &Offsets);
 
-           for (i = 0; i < NX; i++)
-               t[i] = Offsets + i;
+		for (i = 0; i < NX; i++)
+			t[i] = Offsets + i;
 
-           /*  This is here just for fun */
-           uint64_t varsize = adios_expected_var_size(var_ids[block]);
-           /* adios_expected_var_size() works here because NX's value is known by adios at this point */
-           fprintf (stderr, "Temperature block %d is %" PRIu64 " bytes\n", block, varsize);
+		/*  This is here just for fun */
+		uint64_t varsize = adios_expected_var_size(var_ids[block]);
+		/* adios_expected_var_size() works here because NX's value is known by adios at this point */
+		fprintf (stderr, "Temperature block %d is %" PRIu64 " bytes\n", block, varsize);
 
 
-           adios_write(m_adios_file, "temperature", t);
-        }
+		adios_write(m_adios_file, "temperature", t);
+	}
 
-        adios_close (m_adios_file);
+	adios_close (m_adios_file);
 
-        MPI_Barrier (comm);
+	MPI_Barrier (comm);
 
 	adios_finalize (rank);
 
+	free (t);
 	MPI_Finalize ();
 	return 0;
 }
