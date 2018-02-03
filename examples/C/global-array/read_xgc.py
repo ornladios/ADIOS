ca8ca2872a5b52d50956a452ce4028072bbc367e
Liang <liangz@fnal.gov>
2018-02-02 23:03:52 -0600

    a working version of read_xgc.py

--- examples/C/global-array/read_xgc.py
+++ examples/C/global-array/read_xgc.py
@@ -8,37 +8,86 @@ import numpy as np
 
 from threading import Thread
 
-def myfunc(pipename):
-    print('Opening pipes...{0}'.format(pipename))
+import zmq
+
+def read_pipe_data(pipename, bytes_to_read):
+    print('Opening pipe...{0}'.format(pipename) )
     pipe_id  = os.open(pipename, os.O_RDONLY)
     print('Pipe opened...{0}'.format(pipename) )
-    data = os.read(pipe_id, 1024*1024*32)
-    print(len(data))
+
+    while True:
+        data = os.read(pipe_id, 1024*16)
+
+        global field_data
+        global R_data
+        global Z_data
+        global mesh_data
+        global field_bytes
+        global R_bytes
+        global Z_bytes
+        global mesh_bytes
+        if (pipename == field):
+            field_data = np.append(field_data, np.frombuffer(data, dtype=np.float64))
+        elif (pipename == R):
+            R_data = np.append(R_data, np.frombuffer(data, dtype=np.float64))
+        elif (pipename == Z):
+            Z_data = np.append(Z_data, np.frombuffer(data, dtype=np.float64))
+        elif (pipename == mesh):
+            mesh_data = np.append(mesh_data, np.frombuffer(data, dtype=np.int32))
+        else:
+            print("pipe name is wrong")
+
+        bytes_to_read = bytes_to_read - len(data)
+        if bytes_to_read == 0:
+            break
+
+    os.close(pipe_id)
+    print("pipe closed...{0}.".format(pipename))
+   
+def read_pipe_header(pipename):
+    pipe_id  = os.open(pipename, os.O_RDONLY)
+    print('Pipe opened for header...{0}'.format(pipename) )
+    data = os.read(pipe_id, 4)
     os.close(pipe_id)
 
-    global field_data
-    global R_data
-    global Z_data
-    global mesh_data
+    global field_bytes
+    global R_bytes
+    global Z_bytes
+    global mesh_bytes
     if (pipename == field):
-        field_data = np.frombuffer(data, dtype=np.float64)
+        field_bytes = np.frombuffer(data, dtype=np.int32)
+        print('To read {0} bytes from {1}..'.format(field_bytes, pipename))
     elif (pipename == R):
-        R_data = np.frombuffer(data, dtype=np.float64)
+        R_bytes = np.frombuffer(data, dtype=np.int32)
+        print('To read {0} bytes from {1}..'.format(R_bytes, pipename))
     elif (pipename == Z):
-        Z_data = np.frombuffer(data, dtype=np.float64)
+        Z_bytes = np.frombuffer(data, dtype=np.int32)
+        print('To read {0} bytes from {1}..'.format(Z_bytes, pipename))
     elif (pipename == mesh):
-        print('converting mesh data')
-        mesh_data = np.frombuffer(data, dtype=np.int32)
+        mesh_bytes = np.frombuffer(data, dtype=np.int32)
+        print('To read {0} bytes from {1}..'.format(mesh_bytes, pipename))
     else:
         print("pipe name is wrong")
 
-    print("pipe closed...{0}".format(pipename))
-   
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+print('zmq waiting to recv header.')
+message = socket.recv()
+header = np.frombuffer(message, dtype=np.int32)
+bytes_to_read = header[0]
+bytes_to_read_mesh = header[1]
 
 field = '/tmp/MdtmManPipes/field'
 R     = '/tmp/MdtmManPipes/R'
 Z     = '/tmp/MdtmManPipes/Z'
 mesh  = '/tmp/MdtmManPipes/mesh'
+
+field_data = []
+R_data = []
+Z_data = []
+mesh_data = []
 
 try:
     os.mkfifo(field)
@@ -49,13 +98,13 @@ except OSError as oe:
     if oe.errno != errno.EEXIST:
         raise
 
-t1 = Thread(target=myfunc, args=(field,))
+t1 = Thread(target=read_pipe_data, args=(field,bytes_to_read))
 t1.start()
-t2 = Thread(target=myfunc, args=(R,))
+t2 = Thread(target=read_pipe_data, args=(R,bytes_to_read))
 t2.start()
-t3 = Thread(target=myfunc, args=(Z,))
+t3 = Thread(target=read_pipe_data, args=(Z,bytes_to_read))
 t3.start()
-t4 = Thread(target=myfunc, args=(mesh,))
+t4 = Thread(target=read_pipe_data, args=(mesh,bytes_to_read_mesh))
 t4.start()
 
 t1.join()
