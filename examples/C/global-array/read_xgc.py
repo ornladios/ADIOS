45ffae988eb2a5ef1b34072aa5f2903aafd32282
Liang <liangz@fnal.gov>
2018-02-22 16:55:00 -0600

    checkpoint progress (python xgc reader).

--- examples/C/global-array/read_xgc.py
+++ examples/C/global-array/read_xgc.py
@@ -17,6 +17,7 @@ def read_pipe_data(pipename, bytes_to_read):
 
     while True:
         data = os.read(pipe_id, 1024*16)
+        print('Read {0} bytes for {1}'.format(len(data), pipename) )
 
         global field_data
         global R_data
@@ -69,67 +70,68 @@ def read_pipe_header(pipename):
     else:
         print("pipe name is wrong")
 
+step = 0
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
-print('zmq waiting to recv header.')
-message = socket.recv()
-header = np.frombuffer(message, dtype=np.int32)
-bytes_to_read = header[0]
-bytes_to_read_mesh = header[1]
+while True:
+    print('zmq waiting to recv header.')
+    message = socket.recv()
+    header = np.frombuffer(message, dtype=np.int32)
+    bytes_to_read = header[0]
+    bytes_to_read_mesh = header[1]
 
-field = '/tmp/MdtmManPipes/field'
-R     = '/tmp/MdtmManPipes/R'
-Z     = '/tmp/MdtmManPipes/Z'
-mesh  = '/tmp/MdtmManPipes/mesh'
+    print('To read {0} bytes for R/Z/dpot, and {1} bytes for mesh'.format(bytes_to_read, bytes_to_read_mesh))
+    field = '/tmp/MdtmManPipes/field'
+    R     = '/tmp/MdtmManPipes/R'
+    Z     = '/tmp/MdtmManPipes/Z'
+    mesh  = '/tmp/MdtmManPipes/mesh'
 
-field_data = []
-R_data = []
-Z_data = []
-mesh_data = []
+    field_data = []
+    R_data = []
+    Z_data = []
+    mesh_data = []
 
-try:
-    os.mkfifo(field)
-    os.mkfifo(R)
-    os.mkfifo(Z)
-    os.mkfifo(mesh)
-except OSError as oe: 
-    if oe.errno != errno.EEXIST:
-        raise
+    try:
+        os.mkfifo(field)
+        os.mkfifo(R)
+        os.mkfifo(Z)
+        os.mkfifo(mesh)
+    except OSError as oe: 
+        if oe.errno != errno.EEXIST:
+            raise
 
-t1 = Thread(target=read_pipe_data, args=(field,bytes_to_read))
-t1.start()
-t2 = Thread(target=read_pipe_data, args=(R,bytes_to_read))
-t2.start()
-t3 = Thread(target=read_pipe_data, args=(Z,bytes_to_read))
-t3.start()
-t4 = Thread(target=read_pipe_data, args=(mesh,bytes_to_read_mesh))
-t4.start()
+    t1 = Thread(target=read_pipe_data, args=(field,bytes_to_read))
+    t1.start()
+    t2 = Thread(target=read_pipe_data, args=(R,bytes_to_read))
+    t2.start()
+    t3 = Thread(target=read_pipe_data, args=(Z,bytes_to_read))
+    t3.start()
+    t4 = Thread(target=read_pipe_data, args=(mesh,bytes_to_read_mesh))
+    t4.start()
 
-t1.join()
-t2.join()
-t3.join()
-t4.join()
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
 
-"""
-R_data = R_data.astype(np.float64)
-Z_data = Z_data.astype(np.float64)
-field_data = field_data.astype(np.float64)
-mesh_data = mesh_data.astype(np.int32)
-"""
-mesh_data = mesh_data.reshape(mesh_data.size // 3,3)
-plt.figure()
-plt.gca().set_aspect('equal')
-plt.tricontourf(R_data, Z_data, mesh_data, field_data, cmap=plt.cm.jet, levels=np.linspace(-120,120,num=1000))
+    mesh_data = mesh_data.reshape(mesh_data.size // 3,3)
+    plt.figure()
+    plt.gca().set_aspect('equal')
+    plt.tricontourf(R_data, Z_data, mesh_data, field_data, cmap=plt.cm.jet, levels=np.linspace(-120,120,num=1000))
 #plt.plot(R_data, Z_data)
-plt.title('electrostatic potential')
-plt.xlabel('R')
-plt.ylabel('Z')
-plt.savefig('foo.png')
+    plt.title('electrostatic potential')
+    plt.xlabel('R')
+    plt.ylabel('Z')
+    figure_name = "foo_" +  str(step) + ".png"
+    plt.savefig(figure_name)
 
-os.remove(field)
-os.remove(R)
-os.remove(Z)
-os.remove(mesh)
+    print('filename = {0}'.format(figure_name))
+#    os.remove(field)
+#    os.remove(R)
+#    os.remove(Z)
+#    os.remove(mesh)
 
+    step = step + 1
+    message = socket.send_string("ok")
